23fd91bd16fc9b1d715130515ed97e799b2d76b8
dgoore <darius@campgurus.com>
2015-04-11 03:00:40 -0400

    edit and destroy articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-	before_action :find_article, only: [:show]
+	before_action :find_article, only: [:show, :edit, :update, :destroy]
 	before_action :authenticate_user!, except: [:index, :show]
 
 	def index
@@ -26,6 +26,22 @@ class ArticlesController < ApplicationController
 		else
 			render 'new'
 		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
+		end
+	end
+	
+	def destroy
+		@article.destroy
+		redirect_to root_path
 	end
 
 private
