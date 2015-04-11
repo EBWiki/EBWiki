04768fdae44c76ee61263f2b2ee1a410b2247041
dgoore <darius@campgurus.com>
2015-04-11 01:05:33 -0400

    add articles model controller views

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  resources :articles
+  root 'articles#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
