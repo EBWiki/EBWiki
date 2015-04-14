7c7b9077aefd41358b2a91a8b0806670744ac266
dgoore <darius@campgurus.com>
2015-04-14 08:02:02 -0400

    fixing friendly urls

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,7 +3,7 @@ class Article < ActiveRecord::Base
 	belongs_to :category
 	has_paper_trail
 	extend FriendlyId
-	friendly_id :slug
+	friendly_id :title, use: :slugged
 	
 # Avatar uploader using carrierwave
 	mount_uploader :avatar, AvatarUploader
