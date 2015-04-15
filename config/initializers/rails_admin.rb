c2ed5d1e62e5da0a239c97dbbafa7ea39f513e36
dgoore <darius@campgurus.com>
2015-04-15 00:48:04 -0400

    edit rails_admin config to use paper_trail for history

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -12,7 +12,7 @@ RailsAdmin.config do |config|
   # config.authorize_with :cancan
 
   ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
+  config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
 
   ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
 
