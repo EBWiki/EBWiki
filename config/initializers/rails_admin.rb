012e9aa1323334e27a0f309195167cf4978fe921
dgoore <darius@campgurus.com>
2015-04-22 22:04:40 -0400

    authenticating with user admin attribute

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,15 +1,11 @@
 RailsAdmin.config do |config|  
 
-  config.authorize_with do
-    redirect_to root_path unless (current_user && current_user.admin?)
-  end
-
   ### Popular gems integration
 
   ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
+  config.authenticate_with do
+    redirect_to main_app.root_path unless current_user.admin?
+  end
   # config.current_user_method(&:current_user)
 
   ## == Cancan ==
