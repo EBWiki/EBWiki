1e1c3ccd620badd5cb0cb46b09efd212fee3b368
dgoore <darius@campgurus.com>
2015-04-28 11:31:34 -0400

    add some basic rspec for mailer

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ group :development, :test do
   # Spring speeds up development by keeping your application running in the background. Read more: https://github.com/rails/spring
   gem 'spring'
   gem 'rspec-rails', '~> 3.0'
+  gem 'rspec-activemodel-mocks'
 
   gem 'factory_girl_rails'
   # tests and runs specs for you automatically when it detects changes
--- spec/mailers/user_notifier_spec.rb
+++ spec/mailers/user_notifier_spec.rb
@@ -1,5 +1,25 @@
 require "rails_helper"
 
 RSpec.describe UserNotifier, type: :mailer do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'send_update_email' do
+    let(:user) { mock_model User, name: 'John', email: 'john@email.com' }
+    let(:article) {mock_model Article, title: 'John Smith', content: 'some content', state_id: 33}
+    let(:mail) { UserNotifier.send_update_email(user,article) }
+ 
+    it 'renders the subject' do
+      expect(mail.subject).to eql('A new post has been added to BlackOpsWiki')
+    end
+ 
+    it 'renders the receiver email' do
+      expect(mail.to).to eql([user.email])
+    end
+ 
+    it 'renders the sender email' do
+      expect(mail.from).to eql(['blackopswiki0@gmail.com'])
+    end
+ 
+    it 'includes @title' do
+      expect(mail.body.encoded).to match(article.title)
+    end
+  end
 end
