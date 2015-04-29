24d2242eec37ba97a0415ae0afdc8414643247d1
dgoore <darius@campgurus.com>
2015-04-29 13:05:03 -0400

    working out testing issues

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,7 @@
 # This will guess the User class
 FactoryGirl.define do
   factory :user do
+  	email "user@example.com"
     password "password"
     password_confirmation "password"
     factory :admin do
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -35,6 +35,7 @@ RSpec.configure do |config|
 
   # Include Devise test helpers
   config.include Devise::TestHelpers, :type => :controller
+  config.include Controllers::DeviseHelpers, type: :controller
   config.extend ControllerMacros, :type => :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
