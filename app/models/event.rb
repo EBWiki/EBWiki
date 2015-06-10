e03e892546b6b490680538c1101bcb1358ec4647
dgoore <darius@campgurus.com>
2015-06-10 18:27:23 -0400

    adding rspec for events model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ActiveRecord::Base
 	belongs_to :article
+
+	validates :article_id, presence: { message: "An event must belong to an Article." }
+	validates :date, presence: { message: "Please add a date." }
+	validates :title, presence:  { message: "Title (name of the victim) can't be blank." }
+	validates :title, uniqueness:  { scope: :article, message: "We already have an article with this victim" }
 end
