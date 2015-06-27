f61ba32f20b31947c7cf0b2bfa5ef67b71c0bba8
dgoore <darius@campgurus.com>
2015-06-27 14:44:42 -0400

    minor fixes to make update user work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,20 @@ class UsersController < ApplicationController
   def edit
   	@user = User.find(params[:id])
   end
+
+  def update
+    @user = User.find(params[:id])
+	  if @user.update_attributes(user_params)
+	    flash[:success] = "Your profile was updated!"
+			redirect_to @user
+	  else
+	    render 'edit'
+	  end
+  end
+
+private
+
+	def user_params
+		params.require(:user).permit(:name, :description)
+	end
 end
