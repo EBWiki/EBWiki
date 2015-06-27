b2861c1028782be41301bf00c661d7733d2f84d6
dgoore <darius@campgurus.com>
2015-06-26 23:54:45 -0400

    adding lgic to show default image when no image is uploaded

--- app/models/article.rb
+++ app/models/article.rb
@@ -12,7 +12,7 @@ class Article < ActiveRecord::Base
 	accepts_nested_attributes_for :officers, :reject_if => :all_blank, :allow_destroy => true
 	has_many :comments, as: :commentable
 
-	has_paper_trail :only => [:title, :content, :overview, :litigation, :community_atcion]
+	has_paper_trail :only => [:title, :content, :overview, :litigation, :community_action]
 	acts_as_followable
 	extend FriendlyId
 	friendly_id :title, use: [:slugged, :finders]
