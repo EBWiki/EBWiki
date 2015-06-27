c1a01e1bbc291388d7c787eecb2c80ff2828a193
dgoore <darius@campgurus.com>
2015-06-27 14:36:26 -0400

    adding gravatar to profile page. need to configure default avatar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+	def avatar_url(user)
+    default_url = "#{root_url}default-user-icon.png"
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    "http://gravatar.com/avatar/#{gravatar_id}?s=200"
+  end
 end
