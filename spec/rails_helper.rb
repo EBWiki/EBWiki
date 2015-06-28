905ec59af759e2587d54183a1a7080890f5f9d4b
Mark Nyon <mark@grandkru.com>
2015-06-28 15:07:20 -0400

    Fix for article view spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -37,6 +37,8 @@ RSpec.configure do |config|
   # Include Devise test helpers
   config.include Devise::TestHelpers, :type => :controller
   config.extend ControllerMacros, :type => :controller
+  config.include Devise::TestHelpers, :type => :view
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
--- spec/views/articles/show.html.erb_spec.rb
+++ spec/views/articles/show.html.erb_spec.rb
@@ -1,9 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe "articles/show.html.erb", type: :view do
+
   it 'displays content field if content field is not blank' do
   	article = FactoryGirl.create(:article)
-
+  	article.state = FactoryGirl.create(:state)
+  	assign(:article, article)
+  	assign(:officers, article.officers.all)
+  	assign(:commentable, article)
+  	assign(:comments, article.comments)
+  	assign(:comment, Comment.new)
   	render
   	expect(rendered).to match /new article/
   end
