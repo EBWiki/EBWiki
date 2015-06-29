6d985031c93949c6df525e5d9e01a9842cf4a8a5
dgoore <darius@campgurus.com>
2015-06-28 20:45:16 -0400

    adding a few more tests for articles/show subheaders

--- spec/views/articles/show.html.erb_spec.rb
+++ spec/views/articles/show.html.erb_spec.rb
@@ -2,24 +2,60 @@ require 'rails_helper'
 
 RSpec.describe "articles/show.html.erb", type: :view do
 
-  it 'displays content field if content field is not blank' do
-  	article = FactoryGirl.create(:article)
-  	article.state = FactoryGirl.create(:state)
-  	assign(:article, article)
-  	assign(:officers, article.officers.all)
-  	assign(:commentable, article)
-  	assign(:comments, article.comments)
-  	assign(:comment, Comment.new)
-  	render
-  	expect(rendered).to match /new article/
+  it 'displays stub article if content field is present' do
+    article = FactoryGirl.create(:article)
+    article.state = FactoryGirl.create(:state)
+    assign(:article, article)
+    assign(:officers, article.officers.all)
+    assign(:commentable, article)
+    assign(:comments, article.comments)
+    assign(:comment, Comment.new)
+    render
+    expect(rendered).to match /only a stub/m
   end
 
-  # 	describe "check for default images" do
-# 	  it "should have the images" do
-# 	  	article = FactoryGirl.create(:article)
+  it 'displays litigation subheader if litigation text field is present' do
+    article = FactoryGirl.create(:article, content: nil, litigation: 'Legal Action')
+    article.state = FactoryGirl.create(:state)
+    assign(:article, article)
+    assign(:officers, article.officers.all)
+    assign(:commentable, article)
+    assign(:comments, article.comments)
+    assign(:comment, Comment.new)
+    render
+    expect(response.body).to match /Legal Action/m
+  end
 
-# 	  	render article
-# 	    page.should have_css('img', text: "default-user-icon.png")
-# 	  end
-# 	end
+  it 'displays summary subheader if overview text field is present' do
+    article = FactoryGirl.create(:article, content: nil, overview: 'overview text')
+    article.state = FactoryGirl.create(:state)
+    assign(:article, article)
+    assign(:officers, article.officers.all)
+    assign(:commentable, article)
+    assign(:comments, article.comments)
+    assign(:comment, Comment.new)
+    render
+    expect(response.body).to match /Summary/m
+  end
+
+  it 'displays community action subheader if overview text field is present' do
+    article = FactoryGirl.create(:article, content: nil, community_action: 'community text')
+    article.state = FactoryGirl.create(:state)
+    assign(:article, article)
+    assign(:officers, article.officers.all)
+    assign(:commentable, article)
+    assign(:comments, article.comments)
+    assign(:comment, Comment.new)
+    render
+    expect(response.body).to match /Community and Family/m
+  end
+
+  #   describe "check for default images" do
+#     it "should have the images" do
+#       article = FactoryGirl.create(:article)
+
+#       render article
+#       page.should have_css('img', text: "default-user-icon.png")
+#     end
+#   end
 end
