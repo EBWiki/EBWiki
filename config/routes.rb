e5c239251ce5a968dc6e0a2aa48b540dbb0682f8
dgoore <darius@campgurus.com>
2015-07-07 17:21:06 -0400

    File upload to s3 bucket did not work to validate ownership of site and ability to upload to Google Webmaster. Now adding 301 redirect to get sitemaps working

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get '/about', to: 'static#about'
   get '/guidelines', to: 'static#guidelines'
+  get '/sitemap', to: redirect("http://bow-sitemaps.s3.amazonaws.com/sitemaps/sitemap.xml.gz", status: 301)
 
   resources :officers
 
