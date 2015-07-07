267bd96e6ec72dc983fcd8a49f8d98a2723bf432
dgoore <darius@campgurus.com>
2015-07-07 09:58:59 -0400

    adding basic rspec to confirm sitemap_generator does not throw error when called

---
+++ spec/lib/sitemap_generator/interpreter_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe SitemapGenerator::Interpreter do  
+  describe '.run' do
+    it 'does not raise an error' do
+      allow(SitemapGenerator::Sitemap).to receive(:ping_search_engines).and_return true
+      allow(SitemapGenerator::Sitemap).to receive(:create).and_yield
+
+      # Create some test data here with FactoryGirl
+
+      expect { described_class.run }.not_to raise_error
+    end
+  end
+end
