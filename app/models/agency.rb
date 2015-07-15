32de4425dd489d32b8135810dab3fdb36a1982df
dgoore <darius@campgurus.com>
2015-07-15 12:20:35 -0400

    adding validation that an agency must have a state_id to be valid.  Including spec.

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -3,6 +3,7 @@ class Agency < ActiveRecord::Base
   has_many :articles, through: :article_agencies
   belongs_to :state
   validates :name, uniqueness: { message: "We already have an article with this victim" }
+  validates :state_id, presence: { message: "Please specify the state where this agency has headquarters." }
 
   geocoded_by :full_address   # can also be an IP address
 	after_validation :geocode          # auto-fetch coordinates
