a2d4a2ea82644e34d5bb818bc3d919c1c0f99ad4
dgoore <darius@campgurus.com>
2015-07-16 13:21:42 -0400

    adding carrierwave image optimization

--- Gemfile
+++ Gemfile
@@ -95,6 +95,8 @@ gem 'fog'
 gem "mini_magick"
 # file upload solution
 gem 'carrierwave'
+# image optimizer that works with carrierwave
+gem 'carrierwave-imageoptimizer'
 
 #CMS panel for admin
 gem 'rails_admin'
--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,6 +5,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
+  include CarrierWave::ImageOptimizer
 
   # Choose what kind of storage to use for this uploader:
   # storage :file
@@ -36,17 +37,21 @@ class AvatarUploader < CarrierWave::Uploader::Base
     version :large_avatar do
       # returns a 150x150 image
       process :resize_to_fill => [250, 250]
+      process :optimize
     end
     version :medium_avatar do
       # returns a 50x50 image
       process :resize_to_fill => [150, 150]
+      process optimize: [{ quiet: true }]
     end
     version :small_avatar do
       # returns a 35x35 image
       process :resize_to_fill => [35, 35]
+      process optimize: [{ quiet: true }]
     end
     version :thumb do
       process :resize_to_fill => [50, 50]
+      process optimize: [{ quiet: true }]
     end
 
   # Add a white list of extensions which are allowed to be uploaded.
