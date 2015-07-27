b99c255b2462c33397d9409f8bbf1636d3e7a788
dgoore <darius@campgurus.com>
2015-07-27 14:56:38 -0400

    fixing rspec to accommodate new data in the index view

--- spec/factories/state.rb
+++ spec/factories/state.rb
@@ -3,5 +3,6 @@ FactoryGirl.define do
   factory :state do |f|
     f.name "New York"
     f.id 33
+    f.ansi_code "NY"
   end
 end
--- spec/views/articles/index.html.erb_spec.rb
+++ spec/views/articles/index.html.erb_spec.rb
@@ -4,9 +4,11 @@ RSpec.describe "articles/index.html.erb", type: :view do
 
   it "displays all the articles" do
     assign(:articles, Kaminari.paginate_array([
-      FactoryGirl.create(:article, :title => "John Doe"),
-      FactoryGirl.create(:article,:title => "Jimmy Doe")
+      article1 = FactoryGirl.create(:article, :title => "John Doe"),
+      article2 = FactoryGirl.create(:article, :title => "Jimmy Doe")
     ]).page(1))
+    article1.state = FactoryGirl.create(:state)
+    article2.state = article1.state
 
     render
 
