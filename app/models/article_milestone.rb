18d516a6493b2731d15f79b7a148cce0e2348d1e
dgoore <darius@campgurus.com>
2015-08-06 20:49:44 -0400

    adding model specs for article_milestones

--- app/models/article_milestone.rb
+++ app/models/article_milestone.rb
@@ -1,4 +1,5 @@
 class ArticleMilestone < ActiveRecord::Base
 	belongs_to :milestone
 	belongs_to :article
+	validates :date, presence: { message: "Please add a date." }
 end
--- spec/models/article_milestone_spec.rb
+++ spec/models/article_milestone_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ArticleMilestone, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+    it "is invalid without a date" do
+      article_milestone = build(:article_milestone, date: nil)
+      expect(article_milestone).to be_invalid
+    end
+
+    it "returns the correct milestone_id" do
+	  	article_milestone = build(:article_milestone)
+        expect(article_milestone.milestone_id).to equal(1)
+    end
+
+    it "returns the correct description" do
+	  	article_milestone = build(:article_milestone)
+        expect(article_milestone.description).to include "MyText"
+    end
 end
