6ce167547581ae67dc56fe8a228af4fdb1650125
Mark Nyon <mark@grandkru.com>
2015-09-26 09:35:23 -0400

    Added failure case signin feature specs for #234
    - spec/features/admin_spec.rb - Added descriptive comments explaining
      the conditions for success for each feature spec in the file.
      Also, each spec now contains the results of logging into both
      the main site as a user as well as the admin section as an admin.
    
      Also, the following scenarios were added:
    
      * User signin without admin credentials
      * Admin signin without any credentials
      * Admin signin with incorrect credentials

--- spec/features/admin_spec.rb
+++ spec/features/admin_spec.rb
@@ -1,8 +1,16 @@
 require 'rails_helper'
 
   describe User do
-    feature 'Anonymous User' do 
+    feature 'Anonymous User' do
+      # This is a feature spec where a user tries to log onto the
+      # main site as well as the admin section, but does not have
+      # the proper credentials for either section of the site.
+      # In this case the user should get a login error message and not
+      # be able to log into the admin section of the site.
       scenario 'without sign in' do
+        visit new_user_session_path
+        click_button 'Log in'
+        expect(page).to have_content "Invalid email or password"
         visit rails_admin.dashboard_path
         expect(page).to have_content "You are not an admin"
       end
@@ -10,20 +18,46 @@ require 'rails_helper'
 
     feature 'User signs in' do
       let!(:user) { FactoryGirl.create(:user) }
-     
+      # This is a happy path feature spec; this covers the scenario
+      # where a user log into the site section, but
+      # does not have the proper credentials access the admin section,
+      # which it then attempts to access. In this case
+      # the user should not get a login error message, but not
+      # be able to log into the admin section of the site.
       scenario 'without admin credentials' do
         visit new_user_session_path
         fill_in 'Email', with: user.email
         fill_in 'Password', with: user.password
         click_button 'Log in'
+        expect(page).to have_content "Signed in successfully"
         visit rails_admin.dashboard_path
         expect(page).to have_content "You are not an admin"
       end
+
+      # This is a failure feature spec; this covers the scenario
+      # where a user tries to log into the site, but
+      # does not have the proper credentials to do so. In this case
+      # the user should get an error message and not be able to log
+      # into the site
+      scenario 'without any credentials' do
+        visit new_user_session_path
+        click_button 'Log in'
+        expect(page).to have_content "Invalid email or password"
+        visit rails_admin.dashboard_path
+        expect(page).to have_content "You are not an admin"
+      end
+
     end
 
     feature 'Admin signs in' do
       let!(:admin) { FactoryGirl.create(:admin) }
-     
+
+      # This is a happy path feature spec; this covers the scenario
+      # where an admin user logs into the site section and has
+      # the proper credentials access the admin section,
+      # which it then attempts to access. In this case
+      # the user should not get a login error message, and  be able
+      # to log into the admin section of the site.
       scenario 'with admin credentials' do
         visit new_user_session_path
         fill_in 'Email', with: admin.email
@@ -31,6 +65,34 @@ require 'rails_helper'
         click_button 'Log in'
         visit rails_admin.dashboard_path
         expect(page).to have_content "Site Administration"
+      end
+
+      # This is a failure feature spec; this covers the scenario
+      # where a user tries to log into the admin section, but
+      # does not have the proper credentials to do so. In this case
+      # the user should get an error message and not be able to log
+      # into the admin section of the site.
+      scenario 'with incorrect admin credentials' do
+        visit new_user_session_path
+        fill_in 'Email', with: admin.email
+        fill_in 'Password', with: "bad_password"
+        click_button 'Log in'
+        expect(page).to have_content "Invalid email or password"
+        visit rails_admin.dashboard_path
+        expect(page).to have_content "You are not an admin"
+      end
+
+      # This is a failure feature spec; this covers the scenario
+      # where a user tries to log into the admin section without entering
+      # any information on the login form. In this case
+      # the user should get an error message and not be able to log
+      # into the admin section of the site.
+      scenario 'without any credentials' do
+        visit new_user_session_path
+        click_button 'Log in'
+        expect(page).to have_content "Invalid email or password"
+        visit rails_admin.dashboard_path
+        expect(page).to have_content "You are not an admin"
       end
     end
   end
