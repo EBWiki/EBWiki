22463623a0fbb227dd720b6fd62055b5c7e4816a
dgoore <darius@campgurus.com>
2015-09-28 18:42:35 -0400

    adding test to verify that non-logged in follower directed to login screen

--- spec/features/user_follows_article_spec.rb
+++ spec/features/user_follows_article_spec.rb
@@ -22,6 +22,16 @@ feature "User follows an article from show" do
 	end
 end
 
+feature "Non-logged in user attempts to follow an article from show" do
+	let(:article) { FactoryGirl.create(:article) }
+	scenario "User arrives at the article show page and clicks to follow" do
+	  article.state = FactoryGirl.create(:state)
+		visit article_path(article)
+		click_link 'Follow'
+		expect(page).to have_content 'You need to sign in or sign up'
+	end
+end
+
 # feature "User follows an article from followers page" do
 # 	let(:article) { FactoryGirl.create(:article) }
 # 	scenario "User arrives at the article show page and clicks to follow" do
