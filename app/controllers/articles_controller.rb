9056e99750fc3703138d8e2ea9b67c1f4fdadc44
Mark Nyon <mark@minerapp.com>
2016-01-30 03:37:36 -0500

    Adding guard clause to default query; #313

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -16,7 +16,7 @@ class ArticlesController < ApplicationController
   	@articles = Article.by_state(params[:state_id]).search(params[:query], page: params[:page], per_page: 12) if params[:query].present? && params[:state_id].present?
   	@articles = Article.by_state(params[:state_id]).order('date DESC').page(params[:page]).per(12) if !params[:query].present? && params[:state_id].present?
   	@articles = Article.search(params[:query], page: params[:page], per_page: 12) if params[:query].present? && !params[:state_id].present?
-  	@articles = Article.all.order('date DESC').page(params[:page]).per(12)
+  	@articles = Article.all.order('date DESC').page(params[:page]).per(12) if (!params[:query].present? && !params[:state_id].present?)
   end
 
   def new
