98368bd56752daa4ae6a551c2e60e8ec24f86ff4
Mark Nyon <mark@grandkru.com>
2016-01-30 13:36:32 -0500

    Reversed the page_size setting; #313
    - app/controllers/concerns/case_filtering.rb - Reversed the results
      of the ternary operator in the index method.

--- app/controllers/concerns/case_filtering.rb
+++ app/controllers/concerns/case_filtering.rb
@@ -2,7 +2,7 @@ module CaseFiltering
 	extend ActiveSupport::Concern
 
 	def index
-			page_size = (params[:page].present?) ? 1000 : 12
+			page_size = (params[:page].present?) ? 12 : 1000
 	    @articles = Article.by_state(params[:state_id]).search(params[:query], page: params[:page], per_page: page_size) if params[:query].present? && params[:state_id].present?
 	    @articles = Article.by_state(params[:state_id]).order('date DESC').page(params[:page]).per(page_size) if !params[:query].present? && params[:state_id].present?
 	    @articles = Article.search(params[:query], page: params[:page], per_page: page_size) if params[:query].present? && !params[:state_id].present?
