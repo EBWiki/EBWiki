1cadcdb5700e79b73b17b364d63508843dacaac0
Mark Nyon <mark@minerapp.com>
2016-01-30 01:47:46 -0500

    Created Article.by_state query for #313
    Replaced query in articles#index with scope
    
    - app/models/article.rb - Created Article.by_state scope to be used in
      articles_controller.
    
    - app/controllers/articles_controller.rb - Changed articles#index to
      use the new Article.by_state scope.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,145 +1,154 @@
 class ArticlesController < ApplicationController
-	before_action :find_article, only: [:show, :edit, :update, :destroy]
+  before_action :find_article, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show, :history, :followers]
-  #before_action :set_commentable
 
-	def index
-	  if params[:state_id].present?
-	  	@articles_by_state = Article.where(state_id: params[:state_id])
-	    if params[:query].present?
-	      @articles = Article.search("#{params[:query]}", where: {state_id: params[:state_id]}, page: params[:page], per_page: 12)
-	    else
-	      @articles = @articles_by_state.all.order('date DESC').page(params[:page]).per(12)
-	    end
-	  else
-	    if params[:query].present?
-	      @articles = Article.search("#{params[:query]}", page: params[:page], per_page: 12)
-	    else
-	      @articles = Article.all.order('date DESC').page(params[:page]).per(12)
-	    end
-	  end
+  # TODO: This method is currently very complex and needs to be simplified
+  # The first step is identifying all the scenarios that it currently covers
+  # 1. If the action is called with a state_id, then the index should return
+  #    articles selected within the state.
+  # 2. If there is a "query" parameter present, then the index should return
+  #    a paginated result of the full text search, filtered by state
+  # 3. There are "by_state" filters that impact what's returned.
+  # All of theses individual queries in here should be turned into scopes 
+  # and then called safely
+ 
+  def index
+    if params[:state_id].present?
+      @articles_by_state = Article.by_state(params[:state_id])
+      if params[:query].present?
+        @articles = @articles_by_state.search("#{params[:query]}", page: params[:page], per_page: 12)
+      else
+        @articles = @articles_by_state.all.order('date DESC').page(params[:page]).per(12)
+      end
+    else
+      if params[:query].present?
+        @articles = Article.search("#{params[:query]}", page: params[:page], per_page: 12)
+      else
+        @articles = Article.all.order('date DESC').page(params[:page]).per(12)
+      end
+    end
 
     articles_copy = @articles.dup
     @hash = Gmaps4rails.build_markers(articles_copy) do |article, marker|
-		  marker.lat article.latitude
-		  marker.lng article.longitude
-		  marker.infowindow render_to_string(:partial => "/articles/info_window", :locals => { :article => article})
-		end
+      marker.lat article.latitude
+      marker.lng article.longitude
+      marker.infowindow render_to_string(:partial => "/articles/info_window", :locals => { :article => article})
+    end
 
   end
 
-	def new
-		@article = current_user.articles.build
-	end
+  def new
+    @article = current_user.articles.build
+  end
 
-	def show
-		@article = Article.friendly.find(params[:id])
-		@officers = @article.officers.all
-		@commentable = @article
-		@comments = @commentable.comments
-		@comment = Comment.new
-		@subjects = @article.subjects
+  def show
+    @article = Article.friendly.find(params[:id])
+    @officers = @article.officers.all
+    @commentable = @article
+    @comments = @commentable.comments
+    @comment = Comment.new
+    @subjects = @article.subjects
 
     @hash = Gmaps4rails.build_markers(@article.nearby_cases) do |article, marker|
-		  marker.lat article.latitude
-		  marker.lng article.longitude
-		  marker.infowindow render_to_string(:partial => "/articles/info_window", :locals => { :article => article})
-		end
+      marker.lat article.latitude
+      marker.lng article.longitude
+      marker.infowindow render_to_string(:partial => "/articles/info_window", :locals => { :article => article})
+    end
 
-		# Check to make sure all required elements are here
-		unless @article.present? && @article.present?	&& @commentable.present? && @comment.present? &&
-  		@subjects.present?
-  		flash[:error] = "There was an error showing this case. Please try again later"
-  		redirect_to	root_path
-  	end
-	end
+    # Check to make sure all required elements are here
+    unless @article.present? && @article.present?	&& @commentable.present? && @comment.present? &&
+        @subjects.present?
+      flash[:error] = "There was an error showing this case. Please try again later"
+      redirect_to	root_path
+    end
+  end
 
-	def create
-		@article = current_user.articles.build(article_params)
-		# This could be a very expensive query as the userbase gets larger.
-		# TODO: Create a scope to send only to users who have chosen to receive email updates
-		@users = User.all
-		if @article.save
-		    flash[:success] = "Article was created! #{make_undo_link}"
-			redirect_to @article
-		else
-			render 'new'
-		end
-	end
+  def create
+    @article = current_user.articles.build(article_params)
+    # This could be a very expensive query as the userbase gets larger.
+    # TODO: Create a scope to send only to users who have chosen to receive email updates
+    @users = User.all
+    if @article.save
+      flash[:success] = "Article was created! #{make_undo_link}"
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end
 
-	def edit
-	end
+  def edit
+  end
 
-	def followers
-		@article = Article.friendly.find(params[:id])
-	end
+  def followers
+    @article = Article.friendly.find(params[:id])
+  end
 
-	def update
-	  @article = Article.friendly.find(params[:id])
-	  if @article.remove_avatar?
-	  	@article.remove_avatar!
-	  end
-	  if @article.update_attributes(article_params)
-	    flash[:success] = "Article was updated! #{make_undo_link}"
-	    UserNotifier.send_followers_email(@article.followers,@article).deliver_now
-			redirect_to @article
-	  else
-	    render 'edit'
-	  end
-	end
+  def update
+    @article = Article.friendly.find(params[:id])
+    if @article.remove_avatar?
+      @article.remove_avatar!
+    end
+    if @article.update_attributes(article_params)
+      flash[:success] = "Article was updated! #{make_undo_link}"
+      UserNotifier.send_followers_email(@article.followers,@article).deliver_now
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
 
-	def destroy
-		@article.destroy
-	    flash[:success] = "Article was removed! #{make_undo_link}"
-	    UserNotifier.notify_of_removal(@article.followers,@article).deliver_now
-		redirect_to root_path
-	end
+  def destroy
+    @article.destroy
+    flash[:success] = "Article was removed! #{make_undo_link}"
+    UserNotifier.notify_of_removal(@article.followers,@article).deliver_now
+    redirect_to root_path
+  end
 
-	def history
-		@article = Article.friendly.find(params[:id])
-		@versions = @article.versions.order('created_at DESC')
-	end
+  def history
+    @article = Article.friendly.find(params[:id])
+    @versions = @article.versions.order('created_at DESC')
+  end
 
-	def undo
-	  @article_version = PaperTrail::Version.find_by_id(params[:id])
+  def undo
+    @article_version = PaperTrail::Version.find_by_id(params[:id])
 
-	  begin
-	    if @article_version.reify
-	      @article_version.reify.save
-	    else
-	      # For undoing the create action
-	      @article_version.item.destroy
-	    end
-	    flash[:success] = "Undid that! #{make_redo_link}"
-	  rescue
-	    flash[:alert] = "Failed undoing the action..."
-	  ensure
-	    redirect_to root_path
-	  end
-	end
+    begin
+      if @article_version.reify
+        @article_version.reify.save
+      else
+        # For undoing the create action
+        @article_version.item.destroy
+      end
+      flash[:success] = "Undid that! #{make_redo_link}"
+    rescue
+      flash[:alert] = "Failed undoing the action..."
+    ensure
+      redirect_to root_path
+    end
+  end
 
-private
+  private
 
-	def find_article
-		@article = Article.friendly.find(params[:id])
-	end
+  def find_article
+    @article = Article.friendly.find(params[:id])
+  end
 
-	def make_undo_link
-	  view_context.link_to 'Undo that please!', undo_path(@article.versions.last), method: :post
-	end
+  def make_undo_link
+    view_context.link_to 'Undo that please!', undo_path(@article.versions.last), method: :post
+  end
 
-	def make_redo_link
-	  params[:redo] == "true" ? link = "Undo that please!" : link = "Redo that please!"
-	  view_context.link_to link, undo_path(@article_version.next, redo: !params[:redo]), method: :post
-	end
+  def make_redo_link
+    params[:redo] == "true" ? link = "Undo that please!" : link = "Redo that please!"
+    view_context.link_to link, undo_path(@article_version.next, redo: !params[:redo]), method: :post
+  end
 
-	def article_params
-		params.require(:article).permit(:title, :age, :overview, :litigation, :community_action, :agency_id, :category_id, :date, :state_id, :city, :address, :zipcode, :longitude, :latitude, :avatar, :video_url, :remove_avatar, links_attributes: [:id, :url, :_destroy], officers_attributes: [:first_name, :last_name, :title, :avatar, :id, :_destroy], comments_attributes: [:comment, :content, :commentable_id, :commentable_type], subjects_attributes: [:name, :age, :gender_id, :ethnicity_id, :unarmed, :homeless, :veteran, :mentally_ill, :id, :_destroy], article_milestones_attributes:[:date, :description, :milestone_id, :article_id, :id, :_destroy])
-	end
+  def article_params
+    params.require(:article).permit(:title, :age, :overview, :litigation, :community_action, :agency_id, :category_id, :date, :state_id, :city, :address, :zipcode, :longitude, :latitude, :avatar, :video_url, :remove_avatar, links_attributes: [:id, :url, :_destroy], officers_attributes: [:first_name, :last_name, :title, :avatar, :id, :_destroy], comments_attributes: [:comment, :content, :commentable_id, :commentable_type], subjects_attributes: [:name, :age, :gender_id, :ethnicity_id, :unarmed, :homeless, :veteran, :mentally_ill, :id, :_destroy], article_milestones_attributes:[:date, :description, :milestone_id, :article_id, :id, :_destroy])
+  end
 
-	# from the tutorial (https://gorails.com/episodes/comments-with-polymorphic-associations)
-	# why did they set commentable here?
-	def set_commentable
-		@commentable = Article.friendly.find(params[:id])
-	end
+  # from the tutorial (https://gorails.com/episodes/comments-with-polymorphic-associations)
+  # why did they set commentable here?
+  def set_commentable
+    @commentable = Article.friendly.find(params[:id])
+  end
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,48 +1,66 @@
+# This is still called "Article", although its true name is "Case"
+# TODO: Rename this to Case
+# 
 class Article < ActiveRecord::Base
-	belongs_to :user
-	belongs_to :category
-	belongs_to :state
+  # TODO: Clean up relationship section
+  belongs_to :user
+  belongs_to :category
+  belongs_to :state
   has_many :article_agencies
-	has_many :agencies, through: :article_agencies
-	has_many :links
-	accepts_nested_attributes_for :links, :reject_if => :all_blank, :allow_destroy => true
-	has_many :article_officers
-	has_many :officers, through: :article_officers
-	accepts_nested_attributes_for :officers, :reject_if => :all_blank, :allow_destroy => true
-	has_many :comments, as: :commentable
-	has_many :subjects
-	accepts_nested_attributes_for :subjects, :reject_if => :all_blank, :allow_destroy => true
-	has_many :article_milestones
-	has_many :milestones, through: :article_milestones
-	accepts_nested_attributes_for :article_milestones, :reject_if => :all_blank, :allow_destroy => true
+  has_many :agencies, through: :article_agencies
+  has_many :links
+  accepts_nested_attributes_for :links, :reject_if => :all_blank, :allow_destroy => true
+  has_many :article_officers
+  has_many :officers, through: :article_officers
+  accepts_nested_attributes_for :officers, :reject_if => :all_blank, :allow_destroy => true
+  has_many :comments, as: :commentable
+  has_many :subjects
+  accepts_nested_attributes_for :subjects, :reject_if => :all_blank, :allow_destroy => true
+  has_many :article_milestones
+  has_many :milestones, through: :article_milestones
+  accepts_nested_attributes_for :article_milestones, :reject_if => :all_blank, :allow_destroy => true
 
-	has_paper_trail :only => [:title, :overview, :litigation, :community_action]
-	acts_as_followable
-	extend FriendlyId
-	friendly_id :title, use: [:slugged, :finders]
-	searchkick
+  # Paper Trail
+  has_paper_trail :only => [:title, :overview, :litigation, :community_action]
+  # Acts as Follows, for follower functionality
+  acts_as_followable
 
-	validates :date, presence: { message: "Please add a date." }
-	validates :city, presence: { message: "Please add a city." }
-	validates :state_id, presence: { message: "Please specify the state where this incident occurred before saving." }
+  # Friendly ID
+  extend FriendlyId
+  friendly_id :title, use: [:slugged, :finders]
+  
+  # Elasticsearch Gem
+  searchkick
 
-	# Avatar uploader using carrierwave
-	mount_uploader :avatar, AvatarUploader
 
-	geocoded_by :full_address   # can also be an IP address
-	after_validation :geocode          # auto-fetch coordinates
+  # Model Validations
+  validates :date, presence: { message: "Please add a date." }
+  validates :city, presence: { message: "Please add a city." }
+  validates :state_id, presence: { message: "Please specify the state where this incident occurred before saving." }
 
-	def full_address
-		"#{address} #{city} #{state} #{zipcode}"
-	end
 
-	def self.find_by_search(query)
+  # Avatar uploader using carrierwave
+  mount_uploader :avatar, AvatarUploader
+
+  # Geocoding articles
+  geocoded_by :full_address   # can also be an IP address
+  after_validation :geocode          # auto-fetch coordinates
+
+
+  # Scopes
+  scope :by_state, -> {where(state_id: params[:state_id])}
+  
+  def full_address
+    "#{address} #{city} #{state} #{zipcode}"
+  end
+
+  def self.find_by_search(query)
     search(query)
-	end
+  end
 
-	def nearby_cases
-		self.try(:nearbys, 50).try(:order, "distance")
-		#self.nearbys(50).order("distance")
-	end
+  def nearby_cases
+    self.try(:nearbys, 50).try(:order, "distance")
+    #self.nearbys(50).order("distance")
+  end
 
 end
