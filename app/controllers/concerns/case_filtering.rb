83b0c9dc55caf15bade2c9924ba41f33429fd958
dgoore <darius@campgurus.com>
2016-02-16 13:51:28 -0500

    split gem fixes #183
    editing case_filtering to match @trystant pagination fix which somehow
    didn't get passed along in the rebase
    
    also adjusted split.rb to disable ab testing in test environmentt

--- app/controllers/concerns/case_filtering.rb
+++ app/controllers/concerns/case_filtering.rb
@@ -2,7 +2,7 @@ module CaseFiltering
 	extend ActiveSupport::Concern
 	def index
 		finished(:viewed_maps)
-		page_size = (params[:page].present?) ? 12 : 1000
+		page_size = 12
 		@articles = Article.by_state(params[:state_id]).search(params[:query], page: params[:page], per_page: page_size) if params[:query].present? && params[:state_id].present?
 		@articles = Article.by_state(params[:state_id]).order('date DESC').page(params[:page]).per(page_size) if !params[:query].present? && params[:state_id].present?
 		@articles = Article.search(params[:query], page: params[:page], per_page: page_size) if params[:query].present? && !params[:state_id].present?
--- config/initializers/split.rb
+++ config/initializers/split.rb
@@ -6,7 +6,7 @@ Split.configure do |config|
   config.db_failover = true # handle redis errors gracefully
   config.db_failover_on_db_error = proc{|error| Rails.logger.error(error.message) }
   config.allow_multiple_experiments = true # It's fine for me, but might not for you
-  config.enabled = true
+  config.enabled = !Rails.env.test?
 end
 
 # Split::Dashboard.use Rack::Auth::Basic do |username, password|
