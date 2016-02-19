4cba18493e74ad7f9ce9e2c5af47844e8852a8aa
dgoore <darius@campgurus.com>
2016-02-19 14:08:06 -0500

    attempting a view spec for split/dashboard
    rspec can't find the view in the normal folder. I assume it is housed
    somewhere in the gem.

--- spec/views/split/split.html.erb_spec.rb
+++ spec/views/split/split.html.erb_spec.rb
@@ -1,10 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "split.html.erb", type: :view do
+RSpec.describe "/split.html.erb", type: :view do
 
   describe "before authentication" do
-    it 'displays a login screen before authentication' do
-      get 'split'
+    it 'displays a login alert before authentication' do
+      render
       expect(rendered).to match(/you must log in/m)
     end
   end
