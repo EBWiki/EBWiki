6e7923c1b4e7d54783524dd95723f73665c79121
dgoore <darius@campgurus.com>
2016-02-23 12:00:09 -0500

    removing view test for split gem

--- spec/views/split/split.html.erb_spec.rb
+++ spec/views/split/split.html.erb_spec.rb
@@ -1,11 +1,11 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "/split.html.erb", type: :view do
+# RSpec.describe "/split.html.erb", type: :view do
 
-  describe "before authentication" do
-    it 'displays a login alert before authentication' do
-      render
-      expect(rendered).to match(/you must log in/m)
-    end
-  end
-end
+#   describe "before authentication" do
+#     it 'displays a login alert before authentication' do
+#       render
+#       expect(rendered).to match(/you must log in/m)
+#     end
+#   end
+# end
