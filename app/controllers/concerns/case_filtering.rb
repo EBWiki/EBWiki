d771aacf733703ed66faf0b0276282a4e2e5dc88
dgoore <darius@campgurus.com>
2016-03-03 16:11:07 -0500

    Refactoring Split test finish #378
    Moving test finish from case_filtering controller/concern to maps/index
    view seems to do the trick

--- app/controllers/concerns/case_filtering.rb
+++ app/controllers/concerns/case_filtering.rb
@@ -2,7 +2,6 @@ module CaseFiltering
   extend ActiveSupport::Concern
 
   def index
-  	finished(:viewed_maps, reset: false)
     page_size = 12
     @articles = Article.by_state(params[:state_id]).search(params[:query], page: params[:page], per_page: page_size) if params[:query].present? && params[:state_id].present?
     @articles = Article.by_state(params[:state_id]).order('date DESC').page(params[:page]).per(page_size) if !params[:query].present? && params[:state_id].present?
