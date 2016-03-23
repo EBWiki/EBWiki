ce653276af82f9e0e16260a0022c571189ded5b6
dgoore <darius@campgurus.com>
2016-03-22 23:40:50 -0400

    Adding email to the user edit page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,6 @@ class UsersController < ApplicationController
 private
 
 	def user_params
-		params.require(:user).permit(:name, :description, :subscribed)
+		params.require(:user).permit(:name, :email, :description, :subscribed)
 	end
 end
