62b5b2aee2e952709ade4b3043eea2e698b96b8f
Mark Nyon <mark@grandkru.com>
2016-04-23 17:34:33 +0000

    Removed geocoding for the time being
    - app/models/user.rb - Removed geocoding for the user object, until
      an opt-in solution is put together.
    
    Fixes #477

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,6 @@ class User < ActiveRecord::Base
 
   has_many :articles
   has_many :comments
-  geocoded_by :current_sign_in_ip   # can also be a street address
-  before_save :geocode  # auto-fetch coordinates when user logs in
   acts_as_follower
   acts_as_messageable
   extend FriendlyId
