f28a7802f873dbe60213ccc704f30c7521240129
Mark Nyon <mark@grandkru.com>
2016-05-05 07:24:51 -0400

    Moved sendgrid smtp settings to production
    - config/environment.rb - Removed SMTP settings from general environment
    
    - config/environments/production.rb - Moved SMTP settings to production
    
    - config/environments/staging.rb - Set sending email to test to prevent
      sending emails from staging, although this isn't ultimately the best
      solution, since we'll want to test out sending email from staging at
      one point.
    
    Updates #521

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,4 +79,14 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false  
 
+  config.action_mailer.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'blackopswiki.herokuapp.com',
+  :enable_starttls_auto => true
+}
+
 end
