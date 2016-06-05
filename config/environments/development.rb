4c95bd27da7b950cd9bb7bf011bf6baa0a537cbc
dgoore <darius@campgurus.com>
2016-06-05 15:04:44 -0400

    For #578 adding commit summary to update message
    This change will add a little more content to messages to followers.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  # config.action_mailer.smtp_settings = {
+  #   :address        => 'smtp.sendgrid.net',
+  #   :port           => '587',
+  #   :authentication => :plain,
+  #   :user_name      => ENV['SENDGRID_USERNAME'],
+  #   :password       => ENV['SENDGRID_PASSWORD'],
+  #   :domain         => 'blackopswiki.herokuapp.com',
+  #   :enable_starttls_auto => true
+  # }
+
   # Bullet configuration
   Bullet.enable = true
   Bullet.alert = true
