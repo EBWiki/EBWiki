9c5f5795e19118ef084ec68f1e8a61096eaf03af
dgoore <darius@campgurus.com>
2016-06-11 14:46:16 -0400

    for #145 adding unique follower stats
    We had initially reserved this space on the analytics page for email
    update stats, but hat will take more room than expected, so it will be
    moved down the page.

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -17,4 +17,10 @@ class Follow < ActiveRecord::Base
     return (last_month_follows.to_f / (Follow.count-last_month_follows) * 100).round(2)
   end
 
+  def mom_uniq_followers_growth
+    last_month_followers = Follow.where(created_at: 30.days.ago..Time.now).distinct.count('follower_id')
+
+    return (last_month_followers.to_f / (Follow.distinct.count('follower_id')-last_month_followers) * 100).round(2)
+  end
+
 end
