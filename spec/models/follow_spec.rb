9b484b60201f76da5d945f46d95c16fa2d996b88
dgoore <darius@campgurus.com>
2016-06-12 12:51:09 -0400

    for #145 adding spec for distinct followers method

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -1,17 +1,21 @@
 require 'rails_helper'
 
-# RSpec.describe Follow, type: :model do
-
-#   it "is invalid without a name" do
-#     subject = build(:subject, name: nil)
-#     expect(subject).to be_invalid
-#   end
-# end
-
-describe "total follow growth rate" do
-  it "returns the correct percentage increase" do
+RSpec.describe Subject, type: :model do
+  it "follow growth rate returns the correct percentage" do
     follow = FactoryGirl.create(:follow, created_at: 31.days.ago)
     follow2 = FactoryGirl.create(:follow)
+    expect(Follow.first.mom_follows_growth).to eq(100)
+  end
+
+  it "distinct followers is calculated correctly" do
+    follow = FactoryGirl.create(:follow)
+    follow2 = FactoryGirl.create(:follow, follower_id: 2)
+    expect(Follow.distinct.count('follower_id')).to eq(2)
+  end
+
+  it "distinct follower growth rate returns the correct percentage" do
+    follow = FactoryGirl.create(:follow, created_at: 31.days.ago)
+    follow2 = FactoryGirl.create(:follow, follower_id: 2)
     expect(Follow.first.mom_follows_growth).to eq(100)
   end
 end
