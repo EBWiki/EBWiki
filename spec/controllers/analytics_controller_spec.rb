dce75bad86834be595e227a49b9cd1b850e45e0a
dgoore <darius@campgurus.com>
2016-06-13 23:41:34 -0400

    Adding analytics controller test
    trying to address the deficiency highlighted by code climate. so far
    coverage appears unchanged...

--- spec/controllers/analytics_controller_spec.rb
+++ spec/controllers/analytics_controller_spec.rb
@@ -9,11 +9,26 @@ RSpec.describe AnalyticsController, type: :controller do
     end
   end
 
-  describe "GET #index" do
+  describe "GET #index without login" do
     it "redirects to the sign in path" do
       get :index
       subject.should redirect_to new_user_session_path
     end
+  end
+
+  describe "GET #index as admin" do
+    login_admin
+
+    it "renders the :index view" do
+      get :index
+      expect(response).to render_template(:index)
+    end
+
+    # it "populates an array of visits" do
+    #   visit = Factory(:visit)
+    #   get :index
+    #   assigns(:visits).should eq([visit])
+    # end
   end
 
 end
