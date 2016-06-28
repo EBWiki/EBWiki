157f6a2599278f73373fe9c3e07d1db3140fd259
dgoore <darius@campgurus.com>
2016-06-28 13:58:35 -0400

    for #640 requiring auth for editing agencies
    also added back and edit buttons to show view

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -1,5 +1,6 @@
 class AgenciesController < ApplicationController
   before_action :set_agency, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   after_filter "save_my_previous_url", only: [:new, :show]
 
   # GET /agencies
