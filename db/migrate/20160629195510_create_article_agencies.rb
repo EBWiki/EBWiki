a0fd3c67bf9853223ad649c78242d739cc3a422d
dgoore <darius@campgurus.com>
2016-06-30 14:29:26 -0400

    for #640 migration fix
    got an error that can't drop table if doesn't exist. adding conditional
    to create the table unless it already exists. Minor view/controller
    changes as well.

--- db/migrate/20160629195510_create_article_agencies.rb
+++ db/migrate/20160629195510_create_article_agencies.rb
@@ -1,11 +1,12 @@
 class CreateArticleAgencies < ActiveRecord::Migration
   def change
-    drop_table(:article_agencies, if_exists: true)
-    create_table :article_agencies do |t|
-      t.integer :article_id
-      t.integer :agency_id
+    unless table_exists?(:article_agencies)
+      create_table :article_agencies do |t|
+        t.integer :article_id
+        t.integer :agency_id
 
-      t.timestamps null: false
+        t.timestamps null: false
+      end
     end
   end
 end
