3c11beaa21e7ec6c3771db2487bf0a73e6209f49
dgoore <darius@campgurus.com>
2016-07-01 10:34:31 -0400

    adding conditional to create agencies migration

--- db/migrate/20160629194154_create_agencies.rb
+++ db/migrate/20160629194154_create_agencies.rb
@@ -1,21 +1,23 @@
 class CreateAgencies < ActiveRecord::Migration
   def change
-    create_table :agencies do |t|
-      t.string   "name"
-      t.string   "street_address"
-      t.string   "city"
-      t.integer  "state_id"
-      t.string   "zipcode"
-      t.text     "description"
-      t.string   "telephone"
-      t.string   "email"
-      t.string   "website"
-      t.string   "lead_officer"
-      t.datetime "created_at",     null: false
-      t.datetime "updated_at",     null: false
-      t.string   "slug"
+    unless table_exists?(:article_agencies)
+      create_table :agencies do |t|
+        t.string   "name"
+        t.string   "street_address"
+        t.string   "city"
+        t.integer  "state_id"
+        t.string   "zipcode"
+        t.text     "description"
+        t.string   "telephone"
+        t.string   "email"
+        t.string   "website"
+        t.string   "lead_officer"
+        t.datetime "created_at",     null: false
+        t.datetime "updated_at",     null: false
+        t.string   "slug"
 
-      t.timestamps null: false
+        t.timestamps null: false
+      end
     end
   end
 end
