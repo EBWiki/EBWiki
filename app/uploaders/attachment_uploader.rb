795bebcf5a4daec11ddc13942e9a74905a3231e9
dgoore <darius@campgurus.com>
2016-08-02 16:21:34 -0400

    for #426 adding doc links to view
    Now user can click to download documents from links from the show page

--- app/uploaders/attachment_uploader.rb
+++ app/uploaders/attachment_uploader.rb
@@ -36,9 +36,9 @@ class AttachmentUploader < CarrierWave::Uploader::Base
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
-  # def extension_whitelist
-  #   %w(jpg jpeg gif png)
-  # end
+  def extension_whitelist
+    %w(jpg jpeg gif png doc docx pdf)
+  end
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
