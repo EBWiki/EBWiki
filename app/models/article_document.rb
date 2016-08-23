427fc9a59c4836ade0a423b7846de67066ceb18e
campgurus <darius@campgurus.com>
2016-08-23 04:46:11 -0400

    Revert "426 uploader rebase again"

--- app/models/article_document.rb
+++
@@ -1,4 +0,0 @@
-class ArticleDocument < ActiveRecord::Base
-  belongs_to :article
-  belongs_to :document
-end
--- spec/support/fog.rb
+++
@@ -1,4 +0,0 @@
-Fog.mock!
-Fog.credentials_path = Rails.root.join('config/fog_credentials.yml')
-connection = Fog::Storage.new(:provider => 'AWS')
-connection.directories.create(:key => 'my_bucket')
