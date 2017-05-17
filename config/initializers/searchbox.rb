a5fd4b55c6cc3ab1185d4d3435aa4b6afcbb9e00
Mark Nyon <mark@grandkru.com>
2017-05-17 13:56:52 +0000

    Reset elasticsearch to use default port
    - config/initializers/searchbox.rb - The defauult host & port should
      be "http://localhost:9200", not "http://localhost:8081". Fixed
      the initializer to reflect that
    
    Updates #894

--- config/initializers/searchbox.rb
+++ config/initializers/searchbox.rb
@@ -2,7 +2,7 @@
 if ENV["SEARCHBOX_URL"]
 	Searchkick.client = Elasticsearch::Client.new({url: ENV['SEARCHBOX_URL'], logs: true})
 elsif Rails.env == "test"
-	Searchkick.client = Elasticsearch::Client.new({url: "http://localhost:8081", logs: true})
+	Searchkick.client = Elasticsearch::Client.new({url: "http://localhost:9200", logs: true})
 elsif Rails.env == "development"
-	Searchkick.client = Elasticsearch::Client.new({url: "http://localhost:8081", logs: true})
+	Searchkick.client = Elasticsearch::Client.new({url: "http://localhost:9200", logs: true})
 end
