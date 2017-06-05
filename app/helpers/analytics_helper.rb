646b22112eca960f8a7be2f764c323d96530e2ef
Isaac Bell <isaacbell388@gmail.com>
2017-06-05 15:49:33 +0000

    #918 remove_logic_from_analytics_index_page:rearrange helper logic

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -14,47 +14,21 @@ module AnalyticsHelper
   def visits_today_by_referring_domain(visits)
     visits.where(started_at: 1.day.ago..Time.now).group(:referring_domain).count
   end
-  
-  def recently_updated_cases
-    html = ""
     
-    Article.order(updated_at: :desc).first(10).each_with_index do |article, i|
-     html += "<tr>" +
-       "<td>#{ i + 1 }</td>" +
-       "<td>#{ link_to truncate(article.title, length: 14), article }</td>" +
-       "<td><small>#{ article.updated_at.strftime("%m.%e, %l:%M %p") }</small></td>" +
-     "</tr>"
-    end
-    
-    return raw(html)
+  def new_visits
+    Visit.where(started_at: 7.days.ago..Time.now).group(:landing_page).order('count_id DESC').limit(13).count(:id)
   end
   
-  def most_followed_cases
-    html = ""
-    
-    Article.most_recent_by_follower(10).each_with_index do |article, i|
-      html += "<tr>" +
-          "<td>#{ i + 1 }</td>" +
-          "<td>#{ link_to truncate(article.title, length: 25), article }</td>" +
-          "<td>#{ link_to article.follows_count, articles_followers_path(article) }</td>" +
-        "</tr>"
-    end
-    
-    return raw(html)
+  def link_to_article_title(article, length)
+    link_to truncate(article.title, length: length), article
   end
   
-  def weekly_visitors_by_landing_page
-    html = ""
-    
-    Visit.where(started_at: 7.days.ago..Time.now).group(:landing_page).collect{|v| v.find_by_slug(visit[0].split('/').last).present?}.order('count_id DESC').limit(13).count(:id).each_with_index do |visit, i| 
-      html += "<tr>" +
-        "<td>#{ i }</td>" +
-          "<td>#{ link_to truncate(Article.find_by_slug(visit[0].split('/').last).title, length: 25), article_path(Article.find_by_slug(visit[0].split('/').last)) }</td>" +
-          "<td>#{ visit[1] }</td>"
-      "</tr>"
-    end
-    
-    return raw(html)
+  def link_to_article_follower_count(article)
+    link_to article.follows_count, articles_followers_path(article)
+  end
+  
+  def article_updated_at(article)
+    article.updated_at.strftime("%m.%e, %l:%M %p")
   end
   
   def recent_comments_by_creation_time
