056d5ea35f22510fffa574edbcb28f3e0a442025
rlgreen91 <rlgreen91@gmail.com>
2017-08-23 17:59:03 -0500

    Adds test for notify for removal method.

--- spec/mailers/user_notifier_spec.rb
+++ spec/mailers/user_notifier_spec.rb
@@ -31,4 +31,22 @@ RSpec.describe UserNotifier, type: :mailer do
       expect(mail.body.encoded).to match(author.name)
     end
   end
+
+  describe 'notify_of_removal' do
+    let(:follower) { mock_model User, name: 'A Follower', email: 'follower@ebwiki.org' }
+    let(:article) { mock_model Article, title: 'John Smith', content: 'some content', state_id: 33 }
+    let(:mail) { UserNotifier.notify_of_removal([follower], article) }
+
+    it 'renders the subject' do
+      expect(mail.subject).to eql('The @article.title case has been removed from EBWiki')
+    end
+
+    it 'renders the receiver email' do
+      expect(mail.to).to eql([follower.email])
+    end
+
+    it 'renders the sender email' do
+      expect(mail.from).to eql(['EndBiasWiki@gmail.com'])
+    end
+  end
 end
