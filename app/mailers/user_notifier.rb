9c4314b7335c789f498877be01c0f56aabbffeb6
rlgreen91 <rlgreen91@gmail.com>
2017-08-25 10:22:45 -0500

    Removes method send_update_email.

--- app/mailers/user_notifier.rb
+++ app/mailers/user_notifier.rb
@@ -1,13 +1,6 @@
 class UserNotifier < ApplicationMailer
   default :from => 'EndBiasWiki@gmail.com'
 
-  # send a signup email to the user, pass in the user object that   contains the user's email address
-  def send_update_email(user,article)
-    @user = user
-    @article = article # controller getting fat with instance variables
-	  mail( :to => user.email, :subject => 'A new post has been added to EBWiki' )
-  end
-
   def send_followers_email(users, article)
     @article = article
   	users.each do|user|
