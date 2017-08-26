57b28d841eb7917f6a0b7561c9c7ccfafb7bc401
Mark Nyon <mark@grandkru.com>
2017-08-26 12:42:52 +0000

    Removed JSON related code
    - app/controllers/agencies_controller.rb - Removed all JSON related code
      from the controller

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -4,13 +4,11 @@ class AgenciesController < ApplicationController
   after_filter "save_my_previous_url", only: [:new, :show, :edit]
 
   # GET /agencies
-  # GET /agencies.json
   def index
     @agencies = Agency.all
   end
 
   # GET /agencies/1
-  # GET /agencies/1.json
   def show
     @back_url = session[:previous_url]
     @articles = @agency.articles
@@ -26,7 +24,6 @@ class AgenciesController < ApplicationController
   end
 
   # POST /agencies
-  # POST /agencies.json
   def create
     @back_url = session[:previous_url]
     @agency = Agency.new(agency_params)
@@ -34,24 +31,19 @@ class AgenciesController < ApplicationController
     respond_to do |format|
       if @agency.save
         format.html { redirect_to @back_url, notice: 'Agency was successfully created.' }
-        format.json { render :show, status: :created, location: @agency }
       else
         format.html { render :new }
-        format.json { render json: @agency.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /agencies/1
-  # PATCH/PUT /agencies/1.json
   def update
     respond_to do |format|
       if @agency.update(agency_params)
         format.html { redirect_to @agency, notice: 'Agency was successfully updated.' }
-        format.json { render :show, status: :ok, location: @agency }
       else
         format.html { render :edit }
-        format.json { render json: @agency.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -62,7 +54,6 @@ class AgenciesController < ApplicationController
     @agency.destroy
     respond_to do |format|
       format.html { redirect_to agencies_url, notice: 'Agency was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
