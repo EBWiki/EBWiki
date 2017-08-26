54bb56ef0cf4ae2edda7befe96157eb8304a5074
Mark Nyon <mark@grandkru.com>
2017-08-26 12:49:29 +0000

    Removed JSON from organizations_controller
    - app/controllers/organizations_controller.rb - Removed format.json
      code from the controller

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -2,13 +2,11 @@ class OrganizationsController < ApplicationController
   before_action :set_organization, only: [:show, :edit, :update, :destroy]
 
   # GET /organizations
-  # GET /organizations.json
   def index
     @organizations = Organization.all
   end
 
   # GET /organizations/1
-  # GET /organizations/1.json
   def show
   end
 
@@ -22,42 +20,34 @@ class OrganizationsController < ApplicationController
   end
 
   # POST /organizations
-  # POST /organizations.json
   def create
     @organization = Organization.new(organization_params)
 
     respond_to do |format|
       if @organization.save
         format.html { redirect_to @organization, notice: 'Organization was successfully created.' }
-        format.json { render :show, status: :created, location: @organization }
       else
         format.html { render :new }
-        format.json { render json: @organization.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # PATCH/PUT /organizations/1
-  # PATCH/PUT /organizations/1.json
   def update
     respond_to do |format|
       if @organization.update(organization_params)
         format.html { redirect_to @organization, notice: 'Organization was successfully updated.' }
-        format.json { render :show, status: :ok, location: @organization }
       else
         format.html { render :edit }
-        format.json { render json: @organization.errors, status: :unprocessable_entity }
       end
     end
   end
 
   # DELETE /organizations/1
-  # DELETE /organizations/1.json
   def destroy
     @organization.destroy
     respond_to do |format|
       format.html { redirect_to organizations_url, notice: 'Organization was successfully destroyed.' }
-      format.json { head :no_content }
     end
   end
 
