a32b27255afbc2be8db3633d7ecbac1a35ee6e93
Mark Nyon <mark@grandkru.com>
2017-08-27 17:33:15 +0000

    Added oink middleware for debugging
    - config/environments/production.rb - Added Oink middleware to get
      more debugging info for oink command line
    
    Updates #821

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,6 +55,8 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
+  # Configuring middleware to use oink with Heroku
+  config.middleware.use( Oink::Middleware, :logger => Hodel3000CompliantLogger.new(STDOUT))
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
