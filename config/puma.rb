9a68c61f40c5aa272b4f15345fa0cb6cf6158fb5
Mark Nyon <mark@grandkru.com>
2017-10-02 10:32:43 +0000

    Reduced # of workers to 1
    Updates #821

--- config/puma.rb
+++ config/puma.rb
@@ -1,5 +1,5 @@
 workers Integer(ENV['WEB_CONCURRENCY'] || 1)
-threads_count = Integer(ENV['MAX_THREADS'] || 5)
+threads_count = Integer(ENV['MAX_THREADS'] || 1)
 threads threads_count, threads_count
 
 preload_app!
