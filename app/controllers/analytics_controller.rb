d29c1eab150ab2be8deb7045b47fd0e111d40093
Darius Goore <dariusgoore@gmail.com>
2017-11-13 15:35:13 -0500

    For #1099 adding the table of recent editors
    Currently the number of updates in the right coumn of the table links to the user page. ultimately these number should link to a page that shows all o fthe cses updated by that editor. The Name should link to the Editors' user page.

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -11,6 +11,7 @@ class AnalyticsController < ApplicationController
     @visits = Visit.where(started_at: @last_days.days.ago..Time.now)
     @users = User.where(created_at: @last_days.days.ago..Time.now)
     @views = Ahoy::Event.where(name: '$view')
+    @updates = PaperTrail::Version.where("created_at > ?", 30.days.ago)
   end
 
   private
