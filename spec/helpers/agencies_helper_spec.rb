a9ca5ad5b13d82cd3ff9c4b986cb9623781fef9b
Rachel Green <rlgreen91@gmail.com>
2017-12-01 19:30:01 -0600

    Adds spec for agency helper methods.

---
+++ spec/helpers/agencies_helper_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+Rspec.describe AgenciesHelper, :type => :helper do
+  let(:agency) { FactoryGirl.create(:agency, name: 'Spec_Agency') }
+
+  describe "#link_to_name" do
+    it "displays formatted agency name" do
+      expect(helper.link_to_name(agency)).to include('Spec_Agency')
+    end
+  end
+
+  describe "#link_to_edit" do
+    it "displays formatted edit link" do
+      expect(helper.link_to_edit(agency)).to include('/agencies/spec_agency/edit')
+    end
+  end
+end
