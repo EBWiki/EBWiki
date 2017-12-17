549715d43992c775ec99eacb00817f0a923da952
Mark Nyon <mark@grandkru.com>
2017-12-17 23:16:32 +0000

    Fixed Rails application from Blackops -> EBWiki
    - spec/rails_helper.rb - Fixed Rails application name

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module Blackops
+module EBWiki
   class Application < Rails::Application
     config.middleware.insert_before 0, 'Rack::Cors' do
       allow do
