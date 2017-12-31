43e71e5b56e35950f7a2fc8fd112eae4c1a1d3ce
Darius Goore <dariusgoore@gmail.com>
2017-12-30 20:28:38 -0500

    for #1099 removing db calls from analytics view
    moving to analytics_helper instead.

--- app/helpers/analytics_helper.rb
+++ app/helpers/analytics_helper.rb
@@ -10,4 +10,68 @@ module AnalyticsHelper
     # returns list of uniq editors of case updates along woth thenumber of updates each has authored
     case_updates.group(:whodunnit).order('count_id DESC').limit(how_many).count(:id)
   end
+
+  def total_follows_count
+  	Follow.count
+  end
+
+  def mom_visits_growth
+  	Visit.first.mom_visits_growth
+  end
+
+  def cases_updated_last_30_days
+  	Article.first.cases_updated_last_30_days
+  end
+
+  def total_number_of_cases
+  	Article.count
+  end
+
+  def total_cases_mom
+  	Article.first.mom_cases_growth
+  end
+
+  def mom_growth_in_case_updates
+  	Article.first.mom_growth_in_case_updates
+  end
+
+  def mom_follows_growth
+  	Follow.first.mom_follows_growth
+  end
+
+  def unique_followers_count
+  	Follow.distinct.count('follower_id')
+  end
+
+  def mom_uniq_followers_growth
+  	Follow.first.mom_uniq_followers_growth
+  end
+
+  def mom_new_cases_growth
+  	Article.first.mom_new_cases_growth
+  end
+
+  def most_recently_updated_cases
+  	Article.order(updated_at: :desc).first(10)
+  end
+
+  def ten_most_followed_cases
+  	Article.order(follows_count: :desc).first(10)
+  end
+
+  def visits_over_the_past_7_days_by_landing_page
+  	Visit.where(started_at: 7.days.ago..Time.now).group(:landing_page).order('count_id DESC').limit(13).count(:id)
+  end
+
+  def landing_page_was_a_case?
+  	Article.find_by_slug(visit[0].split('/').last).present?
+  end
+
+  def landing_page_from_visit
+  	Article.find_by_slug(visit[0].split('/').last)
+  end
+
+  def editor(user_id)
+  	User.find(user_id)
+  end
 end
