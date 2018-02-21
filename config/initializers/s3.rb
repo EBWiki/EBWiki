9900dba29ceb0047b9c5d9a5170ae711514d8ba9
StlMaris123 <stellamarisnjage@gmail.com>
2018-02-21 18:49:28 +0300

    remove config files from git ignore

---
+++ config/initializers/s3.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    aws_secret_access_key: ENV['AWS_SECRET_KEY_ID']
+    # region: ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+  }
+  config.fog_directory = ENV['S3_BUCKET']
+end
