b6600fa9580185437d14cd994e6ae0fdfdfd901b
Mark Nyon <mark@grandkru.com>
2018-02-27 15:06:04 +0000

    Changed article to case for feature specs & mailers

--- spec/features/editor_edits_article_spec.rb
+++ spec/features/editor_edits_article_spec.rb
@@ -2,12 +2,12 @@
 
 require 'rails_helper'
 
-feature 'Editor edits an article' do
-  let(:article) { FactoryBot.create(:article) }
-  scenario "Editor arrives at the article edit page and sees the subject's name" do
+feature 'Editor edits an case' do
+  let(:this_case) { FactoryBot.create(:case) }
+  scenario "Editor arrives at the case edit page and sees the subject's name" do
     user = FactoryBot.create(:user)
     login_as(user, scope: :user)
-    visit "/articles/#{article.id}/edit"
-    expect(page).to have_content('Edit Case for "' + article.subjects.first.name + '"')
+    visit "/cases/#{this_case.id}/edit"
+    expect(page).to have_content('Edit Case for "' + this_case.subjects.first.name + '"')
   end
 end
