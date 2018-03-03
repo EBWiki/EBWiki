f6232cffcda9caf1bba29e56572e509c391d332d
Camila Bignone <camilabignone@gmail.com>
2018-03-03 12:14:42 -0300

    Fixed control of access to users.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,8 @@ class ApplicationController < ActionController::Base
     http_basic_authenticate_with name: ENV['STAGING_USERNAME'], password: ENV['STAGING_PASSWORD']
   end
 
+  include Pundit
+  before_filter :authenticate_user!
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
---
+++ app/policies/user_policy.rb
@@ -0,0 +1,33 @@
+class UserPolicy
+  attr_reader :current_user, :model
+
+  def initialize(current_user, model)
+    @current_user = current_user
+    @user = model
+  end
+
+  def index?
+    @current_user == @user
+  end
+
+  def show?
+    @current_user == @user
+  end
+
+  def edit?
+    @current_user == @user
+  end
+
+  def new?
+    create?
+  end
+
+  def update?
+    @current_user == @user
+  end
+
+  def destroy?
+    return false if @current_user == @user
+  end
+
+end
