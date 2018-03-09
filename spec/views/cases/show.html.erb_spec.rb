f5b2913d8b6bf0a74c03c62021a820343cfa6b8d
Mark Nyon <mark@grandkru.com>
2018-03-09 21:43:52 +0000

    Split specs into separate blocks; saner helper stub
    - spec/views/cases/show.html.erb_spec.rb - Replaced the
      marker_locations_for singleton monkey_patch (!!) with a saner stub for
      each example group.  Split the top level group into four groups.
    
    Updates #1145

--- spec/views/cases/show.html.erb_spec.rb
+++ spec/views/cases/show.html.erb_spec.rb
@@ -4,65 +4,73 @@ require 'rails_helper'
 
 RSpec.describe 'cases/show.html.erb', type: :view do
   before do
-    controller.singleton_class.class_eval do
-      protected
-
-        def marker_locations_for(_cases)
-          [Case.all]
-        end
-        helper_method :marker_locations_for
-    end
+    allow(view).to receive(:marker_locations_for).and_return([Case.all])
   end
-  describe 'on success' do
-    it 'should not display a content field' do
-      this_case = FactoryBot.create(:case)
-      assign(:case, this_case)
-      assign(:commentable, this_case)
-      assign(:comments, this_case.comments)
-      assign(:comment, Comment.new)
-      assign(:subjects, this_case.subjects)
-      assign(:state_objects, State.all)
-      render
-      expect(rendered).not_to match /only a stub/m
-    end
 
-    it 'displays litigation subheader if litigation text field is present' do
-      this_case = FactoryBot.create(:case, litigation: 'Legal Action')
+  it 'should not display a content field' do
+    this_case = FactoryBot.create(:case)
+    assign(:case, this_case)
+    assign(:commentable, this_case)
+    assign(:comments, this_case.comments)
+    assign(:comment, Comment.new)
+    assign(:subjects, this_case.subjects)
+    assign(:state_objects, State.all)
+    render
+    expect(rendered).not_to match(/only a stub/m)
+  end
+end
 
-      assign(:case, this_case)
-      assign(:commentable, this_case)
-      assign(:comments, this_case.comments)
-      assign(:comment, Comment.new)
-      assign(:subjects, this_case.subjects)
-      assign(:state_objects, State.all)
-      render
-      expect(response.body).to match /Legal Action/m
-    end
+RSpec.describe 'cases/show.html.erb', type: :view do
+  before do
+    allow(view).to receive(:marker_locations_for).and_return([Case.all])
+  end
 
-    it 'displays summary subheader if overview text field is present' do
-      this_case = FactoryBot.create(:case, overview: 'overview text')
+  it 'displays litigation subheader if litigation text field is present' do
+    this_case = FactoryBot.create(:case, litigation: 'Legal Action')
 
-      assign(:case, this_case)
-      assign(:commentable, this_case)
-      assign(:comments, this_case.comments)
-      assign(:comment, Comment.new)
-      assign(:subjects, this_case.subjects)
-      assign(:state_objects, State.all)
-      render
-      expect(response.body).to match /Summary/m
-    end
+    assign(:case, this_case)
+    assign(:commentable, this_case)
+    assign(:comments, this_case.comments)
+    assign(:comment, Comment.new)
+    assign(:subjects, this_case.subjects)
+    assign(:state_objects, State.all)
+    render
+    expect(response.body).to match(/Legal Action/m)
+  end
+end
+RSpec.describe 'cases/show.html.erb', type: :view do
+  before do
+    allow(view).to receive(:marker_locations_for).and_return([Case.all])
+  end
 
-    it 'displays community action subheader if overview text field is present' do
-      this_case = FactoryBot.create(:case, community_action: 'community text')
+  it 'displays summary subheader if overview text field is present' do
+    this_case = FactoryBot.create(:case, overview: 'overview text')
 
-      assign(:case, this_case)
-      assign(:commentable, this_case)
-      assign(:comments, this_case.comments)
-      assign(:comment, Comment.new)
-      assign(:subjects, this_case.subjects)
-      assign(:state_objects, State.all)
-      render
-      expect(response.body).to match /Community and Family/m
-    end
+    assign(:case, this_case)
+    assign(:commentable, this_case)
+    assign(:comments, this_case.comments)
+    assign(:comment, Comment.new)
+    assign(:subjects, this_case.subjects)
+    assign(:state_objects, State.all)
+    render
+    expect(response.body).to match(/Summary/m)
+  end
+end
+RSpec.describe 'cases/show.html.erb', type: :view do
+  before do
+    allow(view).to receive(:marker_locations_for).and_return([Case.all])
+  end
+
+  it 'displays community action subheader if overview text field is present' do
+    this_case = FactoryBot.create(:case, community_action: 'community text')
+
+    assign(:case, this_case)
+    assign(:commentable, this_case)
+    assign(:comments, this_case.comments)
+    assign(:comment, Comment.new)
+    assign(:subjects, this_case.subjects)
+    assign(:state_objects, State.all)
+    render
+    expect(response.body).to match(/Community and Family/m)
   end
 end
