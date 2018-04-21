8b7da6d937197a632fb5d7dd60180b0a852a9363
Rachel Green <rlgreen91@gmail.com>
2018-04-21 16:43:03 -0500

    Refactors welcome email

---
+++ app/helpers/user_notifier_helper.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+#Set of helper methods for composing email messages
+module UserNotifierHelper
+  FOLLOW_CALL_TO_ACTION = 'It is very important that you click to follow one or more cases and allow us to keep you up to date. The more people paying attention, the easier it will be to affect change.'
+  SUBSCRIBER_MESSAGE = "As a newsletter subscriber, you'll receive our general updates periodically."
+  SUBSCRIBER_CALL_TO_ACTION = "#{ActionController::Base.helpers.link_to('Subscribe to our newsletter as well', ENV['MAILCHIMP_LINK'])} for periodic general updates and commentaries on this issue."
+
+  def create_welcome_email_message(user, is_subscribed)
+    followed_cases = user.all_following.count
+    follow_cases_message(followed_cases) + ' ' + subscribe_message(is_subscribed)
+  end
+
+  def follow_cases_message(followed_cases)
+    return FOLLOW_CALL_TO_ACTION if followed_cases.zero?
+    "You have already taken the first step by following " + followed_cases.to_s + "#{pluralize(followed_cases, 'case')} on EBWiki and allowing us to keep you up to date."
+  end
+
+  def subscribe_message(is_subscribed)
+    return SUBSCRIBER_MESSAGE if is_subscribed
+    SUBSCRIBER_CALL_TO_ACTION
+  end
+end
