ade087129afe10920f946f7f6a03c5a3ba4e3459
Rachel Green <rlgreen91@gmail.com>
2018-05-24 21:20:48 -0500

    Refactors code in case show view to use pluralize helper for nearby cases section.  Updates subject model to have association with case instead of article.

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,7 +2,7 @@
 
 # The subject (AKA victim) of the case
 class Subject < ActiveRecord::Base
-  belongs_to :article
+  belongs_to :case
   belongs_to :gender
   belongs_to :ethnicity
   validates :name, presence: { message: 'Name of the victim can\'t be blank.' }
