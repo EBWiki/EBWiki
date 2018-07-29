c831a274c27720a2158a610ccebe6da0aeed520b
Rachel Green <rlgreen91@gmail.com>
2018-07-28 23:17:46 -0500

    Reworks previous jurisdiction migration into two separate migrations.  Switches schema format for application to SQL, and replaces schema.rb with structure.sql

--- db/migrate/20180712191243_add_jurisdiction_to_agencies.rb
+++ db/migrate/20180712191243_add_jurisdiction_to_agencies.rb
@@ -1,17 +1,7 @@
 class AddJurisdictionToAgencies < ActiveRecord::Migration
-  def up
-    execute <<-SQL
+  def change
+    ActiveRecord::Base.connection.execute <<~SQL
       CREATE TYPE jurisdiction AS ENUM ('none', 'local', 'state', 'federal', 'university', 'private');
-    SQL
-
-    add_column :agencies, :jurisdiction, :jurisdiction, index: true
-  end
-
-  def down
-    remove_column :agencies, :jurisdiction
-
-    execute <<-SQL
-      DROP TYPE jurisdiction;
     SQL
   end
 end
