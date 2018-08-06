783c712204b8ca8e61402ab173806f2486ba95c3
StlMaris123 <stellamarisnjage@gmail.com>
2018-08-06 21:46:03 +0300

    update paper_trail to 5.0.0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,8 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
+  
+  before_action :set_paper_trail_whodunnit
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :state_objects
 
