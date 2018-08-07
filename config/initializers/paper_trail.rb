da31f93cb7c85cd12d4f438606434ca0fc210a30
StlMaris123 <stellamarisnjage@gmail.com>
2018-08-07 16:42:36 +0300

    update paper_trail gem

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 PaperTrail::Rails::Engine.eager_load!
-
+PaperTrail.config.track_associations = false
 module PaperTrail
   class Version < ActiveRecord::Base
   end
