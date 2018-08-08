21cc057d9e70a07ac07c9e53377282f2a6fd7e01
rachaelkiteme <rachaelrirrie@gmail.com>
2018-08-08 14:20:32 +0300

    Add field title in case form links section

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,6 +3,7 @@
 # associate Case property
 class Link < ActiveRecord::Base
   belongs_to :case
+  validates :title, presence: true
   validates :url, presence: true
   belongs_to :linkable, polymorphic: true
 end
