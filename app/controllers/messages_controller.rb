b02384f2890ae59158e6df98590fe51e734dd7fc
rachaelkiteme <rachaelrirrie@gmail.com>
2018-08-09 19:42:30 +0300

    add strong param in message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,11 +7,11 @@ class MessagesController < ApplicationController
   def new; end
 
   def create
-    recipients = User.where(id: params['recipients'])
+    recipients = User.where(id: message_params['recipients'])
     conversation = current_user.send_message(
                                               recipients,
-                                              params[:message][:body],
-                                              params[:message][:subject]
+                                              message_params[:message][:body],
+                                              message_params[:message][:subject]
                                             ).conversation
     flash[:success] = 'Message has been sent!'
     redirect_to conversation_path(conversation)
