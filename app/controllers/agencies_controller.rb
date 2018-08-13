7660c502dedc6d1d38c48a4be9ca498b0e5e3962
StlMaris123 <stellamarisnjage@gmail.com>
2018-08-13 22:17:44 +0300

    Add redirect to agencies controller

--- app/controllers/agencies_controller.rb
+++ app/controllers/agencies_controller.rb
@@ -60,6 +60,14 @@ class AgenciesController < ApplicationController
     end
   end
 
+  def after_sign_up_path_for(resource)
+    stored_location_for(resource) || super
+  end
+
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || super
+  end
+
   private
 
   # Use callbacks to share common setup or constraints between actions.
