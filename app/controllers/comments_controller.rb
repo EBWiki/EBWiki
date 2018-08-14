4db4b7b4be16365208a7ba3e7dc8a6bf3763dae5
Dan Uluma <danuluma@gmail.com>
2018-08-15 02:57:14 +0300

    corrected indentation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -21,7 +21,7 @@ class CommentsController < ApplicationController
   end
 
   def after_sign_up_path_for(resource)
-      stored_location_for(resource) || super
+    stored_location_for(resource) || super
   end
 
   def after_sign_in_path_for(resource)
