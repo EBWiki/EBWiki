87505673afb7aafcf2dd4cd04f5dd168aaa91cf9
StlMaris123 <stellamarisnjage@gmail.com>
2018-08-14 17:09:56 +0300

    email uniqueness validation test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,5 +5,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe 'Validations' do
     it { should validate_presence_of(:name).with_message('Please add a name.') }
+    subject { FactoryBot.create(:user) }
+     it { should validate_uniqueness_of(:email).case_insensitive }
   end
 end
