f81d2b2816107caa3c458976ff56f40a74452f81
Dan Uluma <danuluma@gmail.com>
2018-08-15 02:46:51 +0300

    Added redirects for maps controller

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -14,4 +14,13 @@ class MapsController < ApplicationController
       this_case[3] = Case.find_by_id(this_case[0]).avatar.medium_avatar.to_s
     end
   end
+
+def after_sign_up_path_for(resource)
+    stored_location_for(resource) || super
+end
+
+def after_sign_in_path_for(resource)
+  stored_location_for(resource) || super
+end
+
 end
