a08845a62229d5571817e49cda2ca2bb0a746348
Dan Uluma <danuluma@gmail.com>
2018-08-15 03:02:36 +0300

    corrected indentation

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -15,12 +15,11 @@ class MapsController < ApplicationController
     end
   end
 
-def after_sign_up_path_for(resource)
+  def after_sign_up_path_for(resource)
     stored_location_for(resource) || super
-end
+  end
 
-def after_sign_in_path_for(resource)
-  stored_location_for(resource) || super
-end
-
+  def after_sign_in_path_for(resource)
+    stored_location_for(resource) || super
+  end
 end
