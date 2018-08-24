0b5bd4b45c42f6c757ec940174a7f886b0931f67
Dan Uluma <danuluma@gmail.com>
2018-08-24 04:55:33 +0300

    removed 'before_action :state_objects' from application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,7 +15,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_paper_trail_whodunnit
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :state_objects
   before_action :set_paper_trail_whodunnit
 
   helper_method :mailbox, :conversation
