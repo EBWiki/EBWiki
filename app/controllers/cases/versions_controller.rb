11e2d0c0bf0d242de579c411f2adf6c55f80741f
StlMaris123 <stellamarisnjage@gmail.com>
2018-09-04 14:43:50 +0300

    nest vesrions controller in Cases controller

--- app/controllers/cases/versions_controller.rb
+++ app/controllers/cases/versions_controller.rb
@@ -1,24 +1,27 @@
 # frozen_string_literal: true
 
-# versions controller
-class VersionsController < ApplicationController
-  # rubocop:disable MethodLength
+module Cases
+  # versions controller
+  class VersionsController < ApplicationController
+    # rubocop:disable MethodLength
 
-  def revert
-    @case = PaperTrail::Version.find_by_id(params[:id])
-    begin
-      if @case.reify
-        @case.reify.save
-      else
-        # For undoing the create action
-        @case.item.destroy
+    def revert
+      @case = PaperTrail::Version.find_by_id(params[:id])
+      begin
+        if @case.reify
+          @case.reify.save
+        else
+          # For undoing the create action
+          @case.item.destroy
+        end
+        flash[:success] = 'Reverted changes' # {make_redo_link}
+        flash[:reversion] = @version.next
+      rescue StandardError
+        flash[:alert] = 'Failed undoing the action...'
+        redirect_to :back
       end
-      flash[:success] = 'Reverted changes' # {make_redo_link}
-      flash[:reversion] = @version.next
-    rescue StandardError
-      flash[:alert] = 'Failed undoing the action...'
-      redirect_to :back
     end
+
+    # rubocop:enable MethodLength
   end
-  # rubocop:enable MethodLength
 end
