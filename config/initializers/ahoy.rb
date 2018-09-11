d3b0ed1d7267a20d779f40816ec4cd5a6d6f4b7a
StlMaris123 <stellamarisnjage@gmail.com>
2018-09-11 09:20:19 +0300

    Add settings to ahoy initializer #2417

--- config/initializers/ahoy.rb
+++ config/initializers/ahoy.rb
@@ -1,9 +1,9 @@
-
 # frozen_string_literal: true
 
 class Ahoy::Store < Ahoy::DatabaseStore
   # customize here
   Ahoy.api = true
+  Ahoy.user_agent_parser = :device_detector
 
   def exclude?
     bot? || request.ip == '192.168.1.1'
