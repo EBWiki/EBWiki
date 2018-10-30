b7ef17d232bc633b7805c0dbfe7bc472dd216e83
shashank76 <36919726+shashank76@users.noreply.github.com>
2018-10-31 00:47:10 +0530

    Changes in user controller for email update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,19 +2,29 @@
 
 # EBWiki Users controller
 class UsersController < ApplicationController
+  before_action :find_user, only: [:show,:edit,:update,:update_email]
   def show
-    @user = User.find(params[:id])
+    
   end
 
   def edit
-    @user = User.find(params[:id])
+    
   end
 
-  def update
-    @user = User.find(params[:id])
+  def update    
     authorize @user
     if @user.update_attributes(user_params)
       flash[:success] = 'Your profile was updated!'
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def update_email   
+    authorize @user
+    if @user.update_attributes(email_params)
+      flash[:success] = 'Your email was updated!'
       redirect_to @user
     else
       render 'edit'
@@ -33,5 +43,12 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:name, :email, :description, :subscribed)
+  end
+  def find_user
+    @user = User.find(params[:id])
+    redirect_to users_path unless @user
+  end
+  def email_params
+    params.require(:user).permit(:email)
   end
 end
