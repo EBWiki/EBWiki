ab21a52e0d44fb5050da22c6d6456301938632dd
Michael <michael@managedkaos.com>
2018-11-25 11:53:50 -0800

    Add steps for provisioning the database

--- provision.sh
+++ provision.sh
@@ -149,4 +149,8 @@ echo
 echo "##  Starting EBWiki on ${PROJECT_URL}"
 echo '##  Run 'vagrant ssh' to connect to the VM'
 echo '##  Run 'vagrant status' for tips on working with the VM'
+echo
+echo '##  To provision the database, run the following commands:'
+echo '##       vagrant ssh'
+echo '##       source /vagrant/dev_provisions/provision_database.sh'
 cd /vagrant && rails server 2>&1 >> /tmp/ebwiki.log &
