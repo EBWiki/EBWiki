16112f8fc3054d1c33609efe7e3907542440554e
Mark Nyon <mark@grandkru.com>
2018-12-09 01:44:48 +0000

    Fixed rubocop issues in helper

--- app/helpers/user_notifier_helper.rb
+++ app/helpers/user_notifier_helper.rb
@@ -7,10 +7,13 @@ module UserNotifierHelper
     you up to date. The more people paying attention, the easier it will be to affect change.
   FCTA
 
-  SUBSCRIBER_MESSAGE = "As a newsletter subscriber, you'll receive our general updates periodically."
+  SUBSCRIBER_MESSAGE = "As a newsletter subscriber, you'll \
+    receive our general updates periodically."
 
-  subscriber_link = ActionController::Base.helpers.link_to('Subscribe to our newsletter as well', ENV['MAILCHIMP_LINK'])
-  SUBSCRIBER_CALL_TO_ACTION = "#{subscriber_link} for periodic general updates and commentaries on this issue."
+  subscriber_link_copy = 'Subscribe to our newsletter as well', ENV['MAILCHIMP_LINK']
+  subscriber_link = ActionController::Base.helpers.link_to(subscriber_link_copy)
+  SUBSCRIBER_CALL_TO_ACTION = "#{subscriber_link} for periodic \
+   general updates and commentaries on this issue."
 
   def create_welcome_email_message(user, is_subscribed)
     followed_cases = user.all_following.count
@@ -19,7 +22,9 @@ module UserNotifierHelper
 
   def follow_cases_message(followed_cases)
     return FOLLOW_CALL_TO_ACTION if followed_cases.zero?
-    "You have already taken the first step by following #{pluralize(followed_cases, 'case')} on EBWiki and allowing us to keep you up to date."
+    "You have already taken the first step by following \
+      #{pluralize(followed_cases, 'case')} \
+      on EBWiki and allowing us to keep you up to date."
   end
 
   def subscribe_message(is_subscribed)
