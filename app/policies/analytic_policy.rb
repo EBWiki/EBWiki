3b8aa0a23768b5c0cfe07192319ccf804da815c4
StlMaris123 <stellamarisnjage@gmail.com>
2019-01-28 22:12:12 +0300

    fix failing tests

--- app/policies/analytic_policy.rb
+++ app/policies/analytic_policy.rb
@@ -1,19 +1,21 @@
 # frozen_string_literal: true
 
 # Analytic Policy
-AnalyticPolicy = Struct.new(:user, :analytic)
+# rubocop:disable Style/StructInheritance
+class AnalyticPolicy < Struct.new(:user, :analytic)
+  attr_reader :user, :analytic
 
-attr_reader :user, :analytic
+  def initialize(user, analytic)
+    @user = user
+    @analytic = analytic
+  end
 
-def initialize(user, analytic)
-  @user = user
-  @analytic = analytic
+  def index?
+    user.admin? || user.analyst?
+  end
+
+  def show?
+    user.admin? || user.analyst?
+  end
 end
-
-def index?
-  user.admin? || user.analyst?
-end
-
-def show?
-  user.admin? || user.analyst?
-end
+# rubocop:enable Style/StructInheritance
