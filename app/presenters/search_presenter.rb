034cfbca122fb8f8f77ca18dadeeaea43eb7c700
Rachel Green <rlgreen91@gmail.com>
2019-05-03 01:51:42 -0500

    Adds logic to customize search results text and pagination.

---
+++ app/presenters/search_presenter.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+# Presenter for search results page
+class SearchPresenter
+  def initialize(query:, state_id:, count:, view:)
+    @query = query
+    @count = count
+    @view = view
+
+    @state = if state_id.present?
+      State.where(id: state_id).pluck(:name).first
+    else
+      nil
+    end
+  end
+
+  def search_results_text
+    if @query.present?
+      "Your search for \"#{@query}\" #{state_text} has returned #{h.pluralize(@count, 'result')}."
+    else
+      "Your search for cases #{state_text} has returned #{h.pluralize(@count, 'result')}."
+    end
+  end
+
+  def state_text
+    @state.present? ? "in #{@state}" : ''
+  end
+
+  def h
+    @view
+  end
+end
