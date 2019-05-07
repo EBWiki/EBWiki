cef45b75b8fafd6f2d7491e23caa1db0a1190c1e
Michael <michael@managedkaos.com>
2019-05-07 03:05:46 -0700

    Add SEARCHBOX_URL; add fakes3 settings

--- dev_provisions/environment.sh
+++ dev_provisions/environment.sh
@@ -3,13 +3,16 @@ export DEBIAN_FRONTEND=noninteractive
 export BLACKOPS_DATABASE_PASSWORD='ebwiki'
 export DATABASE_DUMP_FILE='latest.dump'
 export NVM_VERSION='v0.33.11'
+export SEARCHBOX_URL='http://127.0.0.1:9200/'
 export MAILCHIMP_API_KEY=''
 export MAILCHIMP_LINK=''
 export MAILCHIMP_LIST_ID=''
-export SEARCHBOX_URL='http://127.0.0.1:9200/'
 export CODECLIMATE_REPO_TOKEN=''
 export AUTOBUS_SNAPSHOT_URL=''
 
 # AWS Credentials for FakeS3.  Please don't change!
 export AWS_ACCESS_KEY_ID='accessKey1'
 export AWS_SECRET_KEY_ID='verySecretKey1'
+export FAKE_S3_KEY='1908855126'
+export FAKE_S3_PORT='4567'
+export FAKE_S3_HOME='/tmp/fakes3'
