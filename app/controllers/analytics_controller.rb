c9402aba7fc2661a4b77b833db4ebd642f40da63
Rachel Green <rlgreen91@gmail.com>
2019-05-25 16:12:13 -0500

    Specifies keyword argument in visit query object.

--- app/controllers/analytics_controller.rb
+++ app/controllers/analytics_controller.rb
@@ -19,7 +19,7 @@ class AnalyticsController < ApplicationController
     @cases_occurring_this_month = Case.most_recent_occurrences 30.days.ago
     @cases_sorted_by_update = Case.sorted_by_update 10
     @cases_sorted_by_followers = Case.sorted_by_followers 10
-    @most_visited_cases = DetermineVisitsToCases.call(VisitQuery.new(@visits_this_week).sorted_by_hits(13))
+    @most_visited_cases = DetermineVisitsToCases.call(VisitQuery.new(@visits_this_week).sorted_by_hits(limit: 13))
     @most_recent_comments = Comment.sorted_by_creation 15
     @mom_new_cases_growth = Statistics.metric_growth(start_metric: CaseQuery.new.most_recent_as_of(date: date_range.start_date),
                                                      end_metric: CaseQuery.new.most_recent_as_of(date: date_range.end_date))
