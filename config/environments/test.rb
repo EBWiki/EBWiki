d9f2bca4885cf5e3bff30f3b1a8e5de64a6c24c7
Rachel Green <rlgreen91@gmail.com>
2019-05-25 21:31:35 -0500

    Adds default url options for mailer testing.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -48,4 +48,7 @@ Rails.application.configure do
     Bullet.bullet_logger = true
     Bullet.raise = true # raise an error if an n+1 query occurs
   end
+
+  # Set default url for mailer testing
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
