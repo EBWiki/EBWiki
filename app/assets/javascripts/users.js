07c12e697c8b9fafbda8873349414df6a2b123ad
Rachel Green <rlgreen91@gmail.com>
2019-08-31 14:04:51 -0500

    Updates logic in conditional for edit profile button.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(){
   const button = document.getElementById('edit-button');
 	var display = button.dataset.display;
-	if (display === 'false'){
+	if (display === 'true'){
 		button.style.display = 'block';
 	}
 });
