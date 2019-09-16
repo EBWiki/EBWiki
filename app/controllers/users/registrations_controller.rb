87bb800a76398095590c0f7c1098dfba02863f9e
Mark Nyon <mnyon@2u.com>
2019-09-16 17:10:22 -0400

    fixing Rubocop issues #3168

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -8,7 +8,6 @@ class Users::RegistrationsController < Devise::RegistrationsController
     OnboardUser.call(resource)
   end
 
-
   # The path used after sign up.
   def after_sign_up_path_for(resource)
     user_path(resource)
