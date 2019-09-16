90ed556db91727bd6b367983b6f366dfb2f6a610
Mark Nyon <mnyon@2u.com>
2019-09-16 08:19:10 -0400

    Removed gotcha from user registration process #3168

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -4,16 +4,10 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   # POST /resource
   def create
-    if gotcha_valid?
-      super
-      OnboardUser.call(resource)
-    else
-      clean_up_passwords resource
-      @minimum_password_length = resource_class.password_length.min if devise_mapping.validatable?
-      flash[:notice] = 'Invalid Captcha'
-      redirect_to '/users/sign_up'
-    end
+    super
+    OnboardUser.call(resource)
   end
+
 
   # The path used after sign up.
   def after_sign_up_path_for(resource)
