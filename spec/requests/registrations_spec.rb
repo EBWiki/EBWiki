6cc05aa50454d116b61d59b15f6a895a10e10172
Mark Nyon <mnyon@cloud9.grandkru.com>
2019-09-22 11:32:01 +0000

    Added success & failure for request specs #3168

--- spec/requests/registrations_spec.rb
+++ spec/requests/registrations_spec.rb
@@ -1,17 +1,47 @@
 RSpec.describe 'Registrations', type: :request do
   describe 'POST /registrations' do
-    it 'works! (now write some real specs)' do
-      get '/'
-      expect(response).to render_template('index')
+    context 'on success' do
+      before do
+        post user_registration_path, params: {
+            user: {
+            name: 'Mark Nyon',
+            email: 'here@there.com',
+            password: '12344321',
+            password_confirmation: '12344321',
+            subscribed: 0
+          }
+        }
+      end
       
-      post user_registration_path, user: {
-          name: 'Mark Nyon',
-          email: 'here@there.com',
-          password: '12344321',
-          password_confirmation: '123443211',
-          subscribed: 0
-      }
-      expect(response).to have_http_status(200)
+      it 'redirects to the users'' home page' do
+        expect(response).to redirect_to('/users/mark-nyon')
+      end
+      
+      it 'creates a new user' do
+        expect(User.last.name).to eq('Mark Nyon')
+      end
+    end
+    
+    context 'on failure' do
+      before do
+        post user_registration_path, params: {
+          user: {
+            name: 'Mark Nyon',
+            email: 'here@there.com',
+            password: '12344321',
+            password_confirmation: '123443211',
+            subscribed: 0
+          }
+        }
+      end
+      
+      it 'renders the registration form again' do
+        expect(response).to render_template('devise/registrations/new')
+      end
+      
+      it 'displays an error message' do
+        expect(response.body).to match('Please review the problems below')
+      end
     end
   end
 end
