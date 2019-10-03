b61726e390fa4f01bdf4f24ccf67f0e66ad355be
StlMaris123 <stellamarisnjage@gmail.com>
2019-10-03 17:26:00 +0300

    fiz rubocop errors

--- app/mailers/case_mailer.rb
+++ app/mailers/case_mailer.rb
@@ -1,10 +1,16 @@
+# frozen_string_literal: true
+
+# Class responsible for sending emails about cases
 class CaseMailer < ApplicationMailer
   default from: 'EndBiasWiki@gmail.com'
- 
- def send_followers_email(users, this_case)
+
+  def send_followers_email(users, this_case)
     @this_case = this_case
     users.each do |user|
-      Rails.logger.info("UserNotifier#send_followers_email: Sending notification to #{user.email} about case #{this_case.title}")
+      # rubocop:disable Metrics/LineLength
+      Rails.logger.info("CaseMailer#send_followers_email: Sending notification to #{user.email} about case #{this_case.title}")
+      # rubocop:enable Metrics/LineLength
+
       mail(to: user.email, subject: "The #{@this_case.title} case has been updated on EBWiki.")
     end
   end
@@ -12,7 +18,10 @@ class CaseMailer < ApplicationMailer
   def send_deletion_email(users, this_case)
     @this_case = this_case
     users.each do |user|
-      Rails.logger.info("UserNotifier#send_deletion_email: Sending notification to #{user.email} about case #{this_case.title}")
+      # rubocop:disable Metrics/LineLength
+
+      Rails.logger.info("CaseMailer#send_deletion_email: Sending notification to #{user.email} about case #{this_case.title}")
+      # rubocop:enable Metrics/LineLength
       mail(to: user.email, subject: "The #{@this_case.title} case has been removed from EBWiki")
     end
   end
