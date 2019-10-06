fc729e1fe97d40b355a544941f80634a3fb56cf7
IlyaOsotov <soten58@yandex.ru>
2019-10-06 11:55:01 +0300

    added users:confirm_all rake task

---
+++ spec/lib/tasks/users_rake_spec.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+describe 'users:confirm_all' do
+  include_context 'rake'
+
+  let(:users) { create_list(:user, 5, confirmed_at: (Time.now - 1.day)) }
+
+  it 'updates confirmed_at of users' do
+    subject.invoke
+    User.all.each do |user|
+      expect(user.confirmed_at).to eq Time.now
+    end
+  end
+end
