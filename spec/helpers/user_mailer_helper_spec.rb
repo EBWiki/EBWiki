c4ddbdd0076854a691386b988ac650497a70ad9d
Rachel Green <rlgreen91@gmail.com>
2019-10-08 22:14:06 -0500

    Missed a merge conflict

--- spec/helpers/user_mailer_helper_spec.rb
+++ spec/helpers/user_mailer_helper_spec.rb
@@ -2,10 +2,9 @@
 
 require 'rails_helper'
 
-<<<<<<< HEAD:spec/helpers/user_notifier_helper_spec.rb
-RSpec.describe UserNotifierHelper, type: :helper do
-  let(:user)                     { FactoryBot.create(:user) }
-  let(:this_case)                { FactoryBot.create(:case) }
+RSpec.describe UserMailerHelper, type: :helper do
+  let(:user)                      { FactoryBot.create(:user) }
+  let(:this_case)                 { FactoryBot.create(:case) }
   let(:follow_call_to_action)    { I18n.t 'notifier.follow_cases' }
   let(:follow_message)           { I18n.t 'notifier.mail_body' }
   let(:subscriber_message)       { I18n.t 'notifier.subscribe_message' }
@@ -13,16 +12,6 @@ RSpec.describe UserNotifierHelper, type: :helper do
     I18n.t('notifier.call_to_action',
            link: ActionController::Base.helpers.link_to(I18n.t('notifier.subscribe'),
                                                         ENV['MAILCHIMP_LINK']))
-  end
-=======
-RSpec.describe UserMailerHelper, type: :helper do
-  let(:user)                      { FactoryBot.create(:user) }
-  let(:this_case)                 { FactoryBot.create(:case) }
-  let(:follow_call_to_action)     { "It is very important that you click to follow one or more cases and allow us to keep\nyou up to date. The more people paying attention, the easier it will be to affect change.\n" }
-  let(:follow_message)            { 'You have already taken the first step by following 1 case on EBWiki and allowing us to keep you up to date.' }
-  let(:subscribe_call_to_action)  { "#{ActionController::Base.helpers.link_to('Subscribe to our newsletter as well', ENV['MAILCHIMP_LINK'])} for periodic general updates and commentaries on this issue." }
-  let(:subscriber_message)        { "As a newsletter subscriber, you'll receive our general updates periodically." }
->>>>>>> c23c0eaab28a495615df0e84956762bbb04189b6:spec/helpers/user_mailer_helper_spec.rb
 
   describe 'create_welcome_email_message' do
     it 'generates the message for an unsubscribed user with no followed cases' do
