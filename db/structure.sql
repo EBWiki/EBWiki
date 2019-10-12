EBWiki/EBWiki

commit 603d8af7356a38e4d1544bec85f93e4f5561791d

Add Dockerfile and associated scripts. (#3001)
* Move provisioning scripts into dev_provisions directory

* Initial check in

* Update for dev provisions

* Initial check in

* Add ENV settings

* Sort ENVs; change CMD to ENTRYPOINT;2Q

* Add database config; remove rake commands

* Initial check in

* Remove nginx install; Use 6.x for elasticsearch

* Move provisioning into Dockerfile

* Remove nginx; Add nodejs install (which is supposed to be in the Dockerfile)

* Update for dev provisions

* Initial check in; for Dockerfile development

* Remove NVM_VERSION env

* Update SEARCHBOX_URL with elasticsearch URL

* Remove project copy to image

* Add PWD as volume for docker run

* Roll up rake commands; Update fakes3 command

* Add SEARCHBOX_URL; add fakes3 settings

* Add database privisioning

* Bring verbose back

* Remove run's dependency on the build target

* Remove database provision from bootstrap

* Move nvm install in to Dockerfile

* Rename the bootstrap script

* Rename the bootstrap script

* Add 'all' target for clean build run

* Add nodejs; remove nvm

* Remove nvm; Use rails server instead of puma

* Add expose for port 3000

* Update target order: build, run, exec

* Use 'rails server' instead of 'puma' to get logs in stdout

* Add ignore file for docker

* Update JRE install to use 'default-jre'

* Fixed rubocop error messages #3001

db/structure.sql
@@ -1982,4 +1982,8 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20190824175709'),
 ('20190825000337');
 
+<<<<<<< HEAD
 
+=======
+INSERT INTO schema_migrations (version) VALUES ('20190422003442');
+>>>>>>> Update for dev provisions

.dockerignore
@@ -1,0 +1,6 @@
+latest.dump
+codeclimate-test-reporter
+./.git
+./tmp
+./log
+./docs

Dockerfile
@@ -1,0 +1,27 @@
+FROM ruby:2.5.5
+
+COPY Gemfile Gemfile.lock /
+
+RUN bundle install && \
+    gem install fakes3 && \
+    apt-get update -qq && \
+    apt-get install apt-transport-https && \
+    wget -q https://artifacts.elastic.co/GPG-KEY-elasticsearch &&\
+    apt-key add GPG-KEY-elasticsearch && \
+    echo "deb https://artifacts.elastic.co/packages/6.x/apt stable main" > \
+        /etc/apt/sources.list.d/elastic-6.x.list && \
+    apt-get update -qq && \
+    apt-get install -qq --no-install-recommends \
+        apt-utils \
+        build-essential \
+        libpq-dev \
+        nodejs \
+        default-jre \
+        postgresql  \
+        redis-server && \
+    apt-get install -qq --no-install-recommends elasticsearch && \
+    mkdir /usr/src/ebwiki
+
+WORKDIR /usr/src/ebwiki
+EXPOSE 3000
+ENTRYPOINT ./dev_provisions/entrypoint.sh

Makefile
@@ -1,0 +1,18 @@
+build:
+	docker build --tag ebwiki/ebwiki .
+
+run:
+	docker run --rm -it --volume $(PWD):/usr/src/ebwiki --publish 3000:3000 --name ebwiki ebwiki/ebwiki
+
+exec:
+	docker exec -it ebwiki bash
+
+stop:
+	docker stop ebwiki
+
+clean:
+	docker image rm ebwiki/ebwiki:latest || echo "clean"
+
+all: clean build run
+
+.PHONY: run build exec stop clean all

Vagrantfile
@@ -9,7 +9,13 @@ Vagrant.configure(VAGRANTFILE_API_VERSION) do |config|
 
   config.vm.define 'ebwiki-server' do |host|
     host.vm.hostname = 'ebwiki-server.local'
-    host.vm.provision 'shell', path: 'provision.sh'
+    host.vm.provision 'shell',
+                      name: 'sys',
+                      path: 'dev_provisions/provision_system.sh'
+    host.vm.provision 'shell',
+                      name: 'app',
+                      path: 'dev_provisions/provision_application.sh',
+                      privileged: false
     host.vm.network 'private_network', ip: "#{guest_ip}"
     host.vm.network 'forwarded_port', guest: '80', host: '3000'
 

dev_provisions/elastic-6.x.list
@@ -1,1 +1,0 @@
-deb https://artifacts.elastic.co/packages/6.x/apt stable main

dev_provisions/entrypoint.sh
@@ -1,0 +1,50 @@
+#!/bin/bash
+source ./dev_provisions/environment.sh
+
+chown postgres ./db/structure.sql
+cp -v ./dev_provisions/database.yml ./config
+
+for i in postgresql redis-server elasticsearch;
+do
+    echo "## Starting $i"
+    service $i start
+done
+
+
+echo -n "## Waiting for elasticsearch..."
+until [ $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://127.0.0.1:9200) -eq 200 ];
+do
+    echo -n ".";
+    sleep 1;
+done
+echo
+
+echo "## Create DB user"
+runuser -l postgres -c "psql -c \"CREATE USER blackops WITH PASSWORD '${BLACKOPS_DATABASE_PASSWORD}';\""
+runuser -l postgres -c "psql -c \"ALTER USER blackops WITH SUPERUSER;\""
+
+for i in create structure:load seed;
+do
+    echo "## Running rake db:${i}"
+    rake db:${i}
+done
+
+echo "## Starting fakes3"
+fakes3 --root ${FAKE_S3_HOME} --port ${FAKE_S3_PORT} --license ${FAKE_S3_KEY} &
+sleep 2
+
+echo '#########################################################'
+echo '##  Environment Summary'
+echo '#########################################################'
+echo "rails   = $(rails -v)"
+echo "ruby    = $(ruby -v)"
+echo "node    = $(node --version)"
+echo "npm     = $(npm -v)"
+echo "java    = $(java -version 2>&1 | grep version)"
+echo "psql    = $(psql --version)"
+echo "redis   = $(redis-server --version | awk '{print $3}')"
+echo "elastic = $(curl -sX GET 'http://localhost:9200')"
+echo '#########################################################'
+
+echo "## Starting the server..."
+bundle exec rails server -b 0.0.0.0 -e development

dev_provisions/environment.sh
@@ -1,17 +1,18 @@
-export EBWIKI_SITEMAP_URL=http://bow-sitemaps.s3.amazonaws.com/sitemaps/sitemap.xml.gz
-export PROJECT_URL='http://192.168.68.68'
-export INSTALL_LOG='/tmp/install.log'
-export DEBIAN_FRONTEND=noninteractive
+export EBWIKI_SITEMAP_URL='http://bow-sitemaps.s3.amazonaws.com/sitemaps/sitemap.xml.gz'
+export DEBIAN_FRONTEND='noninteractive'
 export BLACKOPS_DATABASE_PASSWORD='ebwiki'
 export DATABASE_DUMP_FILE='latest.dump'
-export NVM_VERSION='v0.33.11'
+export SEARCHBOX_URL='http://127.0.0.1:9200/'
+export JAVA_HOME='/usr/lib/jvm/java-8-openjdk-amd64/jre'
 export MAILCHIMP_API_KEY=''
 export MAILCHIMP_LINK=''
 export MAILCHIMP_LIST_ID=''
-export SEARCHBOX_URL=''
 export CODECLIMATE_REPO_TOKEN=''
 export AUTOBUS_SNAPSHOT_URL=''
 
 # AWS Credentials for FakeS3.  Please don't change!
 export AWS_ACCESS_KEY_ID='accessKey1'
 export AWS_SECRET_KEY_ID='verySecretKey1'
+export FAKE_S3_KEY='1908855126'
+export FAKE_S3_PORT='4567'
+export FAKE_S3_HOME='/tmp/fakes3'

commit 22ecdf40717ddb57230ce1f4643671cdbdffa908

Fix first-time database setup
https://github.com/EBWiki/EBWiki/commit/603d8af7356a38e4d1544bec85f93e4f5561791d
introduced some git conflict artifacts to db/structure.sql, which broke
`rake db:setup` (and, presumeably, other db setup tasks.) Removing the
conflict artifacts allows db setup to proceed.

db/structure.sql
@@ -1980,10 +1980,5 @@ INSERT INTO "schema_migrations" (version) VALUES
 ('20190422003442'),
 ('20190815012327'),
 ('20190824175709'),
-('20190825000337');
-
-<<<<<<< HEAD
-
-=======
-INSERT INTO schema_migrations (version) VALUES ('20190422003442');
->>>>>>> Update for dev provisions
+('20190825000337'),
+('20190422003442');