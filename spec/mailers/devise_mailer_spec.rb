eb55e84f0ac4111bb58066d35b0a52d2907af455
K. Stewart <ignitiant@gmail.com>
2019-10-12 13:51:04 -0700

    Address the user by name in email confirmation message
    [#3293]
    From review https://github.com/EBWiki/EBWiki/pull/3371#discussion_r334245776

--- spec/mailers/devise_mailer_spec.rb
+++ spec/mailers/devise_mailer_spec.rb
@@ -15,6 +15,10 @@ RSpec.describe Devise::Mailer, type: :mailer do
       expect(mail.subject).to eql('Confirmation instructions')
     end
 
+    it 'addresses the user' do
+      expect(mail.body.encoded).to include(user.name)
+    end
+
     it 'is polite' do
       expect(mail.body.encoded).to include('Thanks for signing up for EBWiki')
     end
