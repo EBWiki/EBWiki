c3db5196bdcaee30ac15916e314e54813ad440c2
carloslazo <clz1104@gmail.com>
2019-10-12 00:41:36 -0700

    feat: creates remove admin action for mailer

--- app/services/send_admin_email.rb
+++ app/services/send_admin_email.rb
@@ -5,14 +5,28 @@ class SendAdminEmail
   include Service
 
   def call(user:)
+    # returns an array [false, true] or [true, false]
     previous_admin_status = user.previous_changes['admin']
-    send_new_admin_email(user: user) if previous_admin_status[1]
+    send_new_admin_email(user: user)
+    
+    if previous_admin_status[1]
+      send_new_admin_email(user: user)
+    elsif previous_admin_status[1] == false
+      send_removed_admin_email(user: user)
+    end
   end
 
   private
 
   def send_new_admin_email(user:)
-    admin_emails = User.where.not(email: user.email).where(admin: true).pluck(:email)
-    AdminMailer.new_admin_email(new_admin: user, recipients: admin_emails).deliver_now
+    AdminMailer.new_admin_email(new_admin: user, recipients: admin_emails(user: user)).deliver_now
+  end
+  
+  def send_removed_admin_email(user:)
+    AdminMailer.remove_admin_email(removed_admin: user, recipients: admin_emails(user: user)).deliver_now
+  end
+  
+  def admin_emails(user:)
+    User.where.not(email: user.email).where(admin: true).pluck(:email)
   end
 end
