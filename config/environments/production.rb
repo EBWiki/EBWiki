45a9821badaa726cdc82185759850f036b1f3bc4
Andrei <andrei.ungur@mail.mcgill.ca>
2019-10-13 18:37:42 -0400

    Explain why force_ssl setting should remain disabled

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,6 +43,7 @@ Rails.application.configure do
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
+  # This should remain disabled as Cloudflare handles the SSL Configuration [https://github.com/EBWiki/EBWiki/issues/1268]
   # config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
