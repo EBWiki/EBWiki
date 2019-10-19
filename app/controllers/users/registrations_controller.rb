29eeb8db270e4c82f1850e965079e10d17a4d6af
Rachel Green <rlgreen91@gmail.com>
2019-10-19 18:09:40 -0500

    Fixes the redirect for user signups given that confirmation is now required for user accounts.

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -13,7 +13,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
   end
 
   # The path used after sign up.
-  def after_sign_up_path_for(resource)
+  def after_inactive_sign_up_path_for(resource)
     user_path(resource)
   end
 end
