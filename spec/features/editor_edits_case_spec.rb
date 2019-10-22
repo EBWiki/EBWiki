8448c73947af3d7a775df9d5253750f44404a9ca
daniellecroxton <danikrist@gmail.com>
2019-10-21 22:30:57 -0600

    added test for back button on case edit page

--- spec/features/editor_edits_case_spec.rb
+++ spec/features/editor_edits_case_spec.rb
@@ -2,12 +2,24 @@
 
 require 'rails_helper'
 
-feature 'Editor edits an case' do
+feature 'Editor edits a case' do
   let(:this_case) { FactoryBot.create(:case) }
+  let(:user) { FactoryBot.create(:user) }
   scenario 'Editor arrives at the case edit page and sees the subject''s name' do
-    user = FactoryBot.create(:user)
+
     login_as(user, scope: :user)
     visit "/cases/#{this_case.id}/edit"
     expect(page).to have_content('Editing Case ' + this_case.title)
   end
+
+  scenario 'Editor arrives at the case edit page and selects back button' do
+
+    login_as(user, scope: :user)
+    visit "/cases/#{this_case.id}/edit"
+    expect(page).to have_content('Back to Case')
+    click_button('Back to Case')
+    expect(page).to eq "/cases/#{this_case.id}"
+  end
+
+
 end
