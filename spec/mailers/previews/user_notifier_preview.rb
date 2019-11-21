9fdcd68f287ea5b1b6fb23eec03d5c4dc1315bc2
Mark Nyon <mark@grandkru.com>
2019-11-20 22:25:10 -0500

    Removed UserNotifier & related code

--- spec/mailers/previews/user_notifier_preview.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-# Preview all emails at http://localhost:3000/rails/mailers/user_notifier
-class UserNotifierPreview < ActionMailer::Preview
-  def send_followers_email
-    CaseMailer.send_followers_email(users: [User.first], this_case: Case.last)
-  end
-end
