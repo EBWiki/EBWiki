c7f6a518339a12da361046c121e39c82382cefee
Mark Nyon <mark@grandkru.com>
2019-12-01 05:59:59 +0000

    Removed maps helper and view #851

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,28 +11,6 @@ module ApplicationHelper
     "http://gravatar.com/avatar/#{gravatar_id}?s=#{size}"
   end
 
-  def filter
-    if params[:controller] == 'maps'
-      '/maps/index'
-    else
-      'cases'
-    end
-  end
-
-  def marker_locations_for(cases)
-    return nil if cases.blank?
-
-    @hash = Gmaps4rails.build_markers(cases) do |this_case, marker|
-      marker.lat this_case[1]
-      marker.lng this_case[2]
-      marker.infowindow controller.render_to_string(
-        partial: '/cases/info_window',
-        locals: { this_case: this_case }
-      )
-    end
-    @hash
-  end
-
   def active_page_link(page, remote)
     content_tag :a, page, remote: remote, rel: link_text(page)
   end
