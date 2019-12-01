41f9f13c114b949dedfd3683dfd123c25e564096
Mark Nyon <mark@grandkru.com>
2019-12-01 05:13:14 +0000

    Removed gmaps reference & markerclusterer files #851

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require gmaps/google
 //= require jquery
 //= require ahoy
 //= require jquery.turbolinks
