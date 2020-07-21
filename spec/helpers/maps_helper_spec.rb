e2b9a4a7bec85d8b2a6a25c012414465067bdf8a
Mark Nyon <mark@grandkru.com>
2020-07-21 06:05:46 +0000

    Initial maps controller #851

--- spec/helpers/maps_helper_spec.rb
+++ spec/helpers/maps_helper_spec.rb
@@ -5,11 +5,18 @@ require 'rails_helper'
 RSpec.describe MapsHelper, type: :helper do
   describe '#fetch_cases' do
     it 'fetches cases from the database if they do not exist in the cache' do
+<<<<<<< HEAD
       $redis.set('cases', '')
       expect($redis.get('cases')).to be_empty
       this_case = FactoryBot.create(:case)
       helper.fetch_cases
       expect($redis.get('cases')).to eql([this_case['latitude'], this_case['longitude']].to_json)
+=======
+      expect($redis.get('cases')).to be_nil
+      FactoryBot.create(:case)
+      helper.fetch_cases
+      expect(JSON.parse($redis.get('cases')).size).to eq(1)
+>>>>>>> Initial maps controller #851
     end
 
     it 'fetches cases from the cache if they are present' do
