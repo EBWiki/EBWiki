9f6dd59ff0d9f10ff25bbac982b6bfa9381142af
Mark Nyon <mark@grandkru.com>
2020-09-07 09:21:07 -0400

    3648 add here libraries to codebase (#3653)
    * adding JS libraries & map views #3648
    
    * Fixed spec to match page expectations #3648

---
+++ spec/views/maps/index.html.erb_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'maps/index.html.erb', type: :view do
+  it 'displays all the articles' do
+    FactoryBot.create(:case, title: 'John Doe')
+    FactoryBot.create(
+      :case, title: 'Jimmy Doe', state: State.where(ansi_code: 'NY').first
+    )
+    assign(:cases, Kaminari.paginate_array(
+        Case.pluck(:id,
+                   :latitude,
+                   :longitude,
+                   :avatar,
+                   :title,
+                   :overview)
+      ).page(1))
+    render
+
+    expect(rendered).to match(/Map at a specified location/m)
+  end
+end
