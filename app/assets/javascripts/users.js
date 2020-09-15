571438733865aa623be62ff6f8cd4c229de5f0f3
Mark Nyon <mark@grandkru.com>
2020-09-15 11:00:58 +0000

    Reverted comments #3649

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,7 +1,7 @@
-// document.addEventListener("DOMContentLoaded", function(){
-//   const button = document.getElementById('edit-button');
-// 	var display = button.dataset.display;
-// 	if (display === 'true'){
-// 		button.style.display = 'block';
-// 	}
-// });
+document.addEventListener("DOMContentLoaded", function(){
+  const button = document.getElementById('edit-button');
+	var display = button.dataset.display;
+	if (display === 'true'){
+		button.style.display = 'block';
+	}
+});
