912d7646024963934ba0cd5909876f144718ef8b
Michael <michael@managedkaos.com>
2020-09-20 21:12:46 -0700

    Add logic to terminate warm up if the server takes too long

--- dev_provisions/prewarm.sh
+++ dev_provisions/prewarm.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 echo "## Warming up the server..."
-until [ $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://localhost:3000) -ne 000 ];
+count=0
+until [ $(curl -o /dev/null --silent --head --write-out '%{http_code}\n' http://localhost:3000) -ne 000 ] | [ ${count} -gt 360 ];
 do
-    echo -n ".";
+    echo -n ".${count}";
+    count=$((count+1))
     sleep 1;
 done
 echo
-echo "## Prewarm is complete!"
+if [ $count -gt 360 ];
+then
+    echo "## Ending warm up because the server took too long.  There might be a problem with the server. :("
+    exit $count
+else
+    echo "## Warm up is complete!"
+fi
