7c55cf3a7b61bd048b84b2b451c7c82161270a5f
Michael <michael@managedkaos.com>
2020-09-20 21:26:52 -0700

    replace grep with  option; count passes and fails

--- dev_provisions/run_tests.sh
+++ dev_provisions/run_tests.sh
@@ -1,10 +1,22 @@
 #!/bin/bash -e
-
 echo "## Running tests for seed cases..."
+
+pass=0
+fail=0
+
 for i in sven-svensson janez-novak max-mustermann janina-kowalska chichiko-bendeliani mario-rossi;
 do
-    curl --max-time 60 -sLk -o /dev/null \
+    curl --fail --max-time 60 -sLk -o /dev/null \
         -w "%{time_total} %{num_redirects} %{http_code} %{url_effective}\n" \
-        "http://localhost:3000/cases/${i}" | grep 200;
+        "http://localhost:3000/cases/${i}" \
+        && pass=$((pass+1)) || fail=$((fail+1));
 done
+
 echo "## Tests for seed cases complete!"
+echo "Pass: $pass"
+echo "Fail: $fail"
+
+if [ $fail -gt 0 ];
+then
+    exit $fail;
+fi
