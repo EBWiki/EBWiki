aeeec39bfc94eeb8f4bb81e64cc2a4cac6229646
Michael <michael@managedkaos.com>
2020-09-21 12:42:02 -0700

    Add count for total number of tests

--- dev_provisions/run_tests.sh
+++ dev_provisions/run_tests.sh
@@ -3,6 +3,7 @@ echo "## Running tests for seed cases..."
 
 pass=0
 fail=0
+count=0
 
 for i in sven-svensson janez-novak max-mustermann janina-kowalska chichiko-bendeliani mario-rossi;
 do
@@ -10,11 +11,12 @@ do
         -w "%{time_total} %{num_redirects} %{http_code} %{url_effective}\n" \
         "http://localhost:3000/cases/${i}" \
         && pass=$((pass+1)) || fail=$((fail+1));
+    count=$((count+1))
 done
 
 echo "## Tests for seed cases complete!"
-echo "Pass: $pass"
-echo "Fail: $fail"
+echo "Pass: $pass / $count"
+echo "Fail: $fail / $count"
 
 if [ $fail -gt 0 ];
 then
