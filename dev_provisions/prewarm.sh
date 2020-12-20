bb3673329dffeb2a13469bed19d32edb7dac558a
Mark Nyon <mark@grandkru.com>
2020-12-20 03:29:15 -0500

    Tried another way to call PWD

--- dev_provisions/prewarm.sh
+++ dev_provisions/prewarm.sh
@@ -14,13 +14,13 @@ do
         echo "## Ending warm up because the server took too long or stopped unexpectedly."
         echo ""
         echo "## To debug, start the server with the following commands and open an issue with the output:"
-        echo "docker run --volume \"$(GITHUB_WORKSPACE)\":/usr/src/ebwiki --publish 3000:3000 --name ebwiki ebwiki/ebwiki"
+        echo "docker run --volume .:/usr/src/ebwiki --publish 3000:3000 --name ebwiki ebwiki/ebwiki"
         echo ""
         echo "## You can also use these commands to build the server image from scratch and then run it:"
         echo "docker container rm ebwiki"
         echo "docker image rm ebwiki/ebwiki:latest"
         echo "docker build --tag ebwiki/ebwiki ."
-        echo "docker run --volume \"$(GITHUB_WORKSPACE)\":/usr/src/ebwiki --publish 3000:3000 --name ebwiki ebwiki/ebwiki"
+        echo "docker run --volume .:/usr/src/ebwiki --publish 3000:3000 --name ebwiki ebwiki/ebwiki"
         exit $count
     fi
 done
