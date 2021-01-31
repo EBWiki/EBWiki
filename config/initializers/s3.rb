e8b3ff5e144efe1ff7c01cfb3d3f1ed892c1cf0e
Rachel Green <rlgreen91@gmail.com>
2021-01-31 02:02:15 -0600

    Updates code and docs for streamlined dev and test setup

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,11 +1,19 @@
 # frozen_string_literal: true
 
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-    aws_secret_access_key: ENV['AWS_SECRET_KEY_ID']
-    # region: ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
-  }
-  config.fog_directory = ENV['S3_BUCKET']
+  if Rails.env.development?
+    config.storage = :file
+  else
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+      aws_secret_access_key: ENV['AWS_SECRET_KEY_ID']
+      # region: ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+    }
+    config.storage = :fog
+    config.fog_directory = ENV['S3_BUCKET']
+
+    if Rails.env.test?
+      config.enable_processing = false
+    end
 end
