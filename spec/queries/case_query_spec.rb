b64658b3e5120a120f368d69d71f72cd9d8bb306
Daniel Dao <daniel.vu.dao@gmail.com>
2021-03-12 07:45:09 -0800

    Fix spec

--- spec/queries/case_query_spec.rb
+++ spec/queries/case_query_spec.rb
@@ -10,7 +10,7 @@ RSpec.describe CaseQuery do
     it 'returns, given a date, the cases available that day occurring within 30 days prior' do
       case_record = FactoryBot.create(:case, date: 20.days.ago)
       case_record.update(created_at: 17.days.ago)
-      expect(most_recent_as_of.pluck(:title).first).to eq _case.title
+      expect(most_recent_as_of.pluck(:title).first).to eq case_record.title
     end
   end
 
@@ -21,7 +21,7 @@ RSpec.describe CaseQuery do
     it 'returns, given a date, the cases created by that date' do
       case_record = FactoryBot.create(:case)
       case_record.update(created_at: 17.days.ago)
-      expect(created_by.pluck(:title).first).to eq _case.title
+      expect(created_by.pluck(:title).first).to eq case_record.title
     end
   end
 
@@ -32,7 +32,7 @@ RSpec.describe CaseQuery do
     it 'returns, given a date, the cases available that day and updated within 30 days prior' do
       case_record = FactoryBot.create(:case, date: 20.days.ago)
       case_record.update(created_at: 17.days.ago, updated_at: 17.days.ago)
-      expect(recently_updated_as_of.pluck(:title).first).to eq _case.title
+      expect(recently_updated_as_of.pluck(:title).first).to eq case_record.title
     end
   end
 end
