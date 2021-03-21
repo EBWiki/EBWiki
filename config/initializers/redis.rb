beb85f29fc0fe5aafd8708c4b519da56d0882fc0
Mark Nyon <me@mnyon.com>
2021-03-21 19:59:37 -0400

    Fixed Rubocop offenses #3863

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -3,11 +3,11 @@
 # config/initializers/redis.rb
 
 $redis = if Rails.env.test?
-  Redis::Namespace.new('ebwiki', redis: MockRedis.new)
-elsif Rails.env.production?
-  Redis::Namespace.new('ebwiki', redis: Redis.new(url: ENV['REDISTOGO_URL']))
-else
-  Redis::Namespace.new('ebwiki', redis: Redis.new('localhost',
-                                                 port: 6379,
-                                                 db: 0))
- end
+           Redis::Namespace.new('ebwiki', redis: MockRedis.new)
+         elsif Rails.env.production?
+           Redis::Namespace.new('ebwiki', redis: Redis.new(url: ENV['REDISTOGO_URL']))
+         else
+           Redis::Namespace.new('ebwiki', redis: Redis.new('localhost',
+                                                           port: 6379,
+                                                           db: 0))
+         end
