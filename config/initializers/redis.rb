dfd145a8946a260a38b7b70af4ef53c593b61df2
mnyon-grandkru <me@mnyon.com>
2021-03-21 20:37:01 -0400

    Changed REDIS initialization #3863 (#3996)
    * Changed REDIS initialization #3863
    
    * Fixed Rubocop offenses #3863
    
    Co-authored-by: Mark Nyon <mark@grandkru.com>

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -4,9 +4,10 @@
 
 $redis = if Rails.env.test?
            Redis::Namespace.new('ebwiki', redis: MockRedis.new)
+         elsif Rails.env.production?
+           Redis::Namespace.new('ebwiki', redis: Redis.new(url: ENV['REDISTOGO_URL']))
          else
-           Redis::Namespace.new('ebwiki',
-                                redis: Redis.new(host: ENV['REDISTOGO_URL'] || 'localhost',
-                                                 port: 6379,
-                                                 db: 0))
+           Redis::Namespace.new('ebwiki', redis: Redis.new('localhost',
+                                                           port: 6379,
+                                                           db: 0))
          end
