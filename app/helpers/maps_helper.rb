49818f2f7d82de80db1cd48a6dc4423047c2a9c7
Mark Nyon <me@mnyon.com>
2021-03-24 16:49:53 -0400

    Fix rubocop offenses #3862

--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -1,21 +1,23 @@
 # frozen_string_literal: true
 
 # Fetches the cases using Redis cache
-# rubocop:disable Metrics/AbcSize
 module MapsHelper
   def fetch_cases
     cases = $redis.get('cases')
 
     if cases.blank?
-      cases = Case.all.select do |this_case|
-        this_case.latitude.present? && this_case.longitude.present?
-      end
-      cases = cases.map { |case_location| [case_location.latitude, case_location.longitude] }.to_json
-
-      $redis.set('cases', cases)
+      case_locations = location_data(Case.with_location)
+      $redis.set('cases', case_locations)
       $redis.expire('cases', 2.hour.to_i)
     end
-    JSON.load(cases)
+    cases
   end
-  # rubocop:enable Metrics/AbcSize
+
+  private
+
+  def location_data(cases)
+    cases.map do |case_location|
+      [case_location.latitude, case_location.longitude]
+    end.flatten.to_json
+  end
 end
