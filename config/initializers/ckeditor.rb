ba0ef229efa9a478e25d37369a55d30194169155
Rachel Green <rlgreen91@gmail.com>
2021-04-23 20:56:49 -0500

    Upgrades ckeditor gem (#4012)

--- config/initializers/ckeditor.rb
+++ config/initializers/ckeditor.rb
@@ -2,5 +2,5 @@
 
 Ckeditor.setup do |config|
   # //cdn.ckeditor.com/<version.number>/<distribution>/ckeditor.js
-  config.cdn_url = '//cdn.ckeditor.com/4.5.6/standard/ckeditor.js'
+  config.cdn_url = '//cdn.ckeditor.com/4.6.1/standard/ckeditor.js'
 end
