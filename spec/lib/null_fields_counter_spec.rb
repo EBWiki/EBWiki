93d5b7902109b46aa2e4cb5b996351363fb77913
Mark Nyon <me@mnyon.com>
2021-09-22 00:40:54 -0400

    Included NullFieldsCounter in DummyClass #4037

--- spec/lib/null_fields_counter_spec.rb
+++ spec/lib/null_fields_counter_spec.rb
@@ -2,18 +2,23 @@
 
 require 'rails_helper'
 
+class CounterClass
+  include NullFieldsCounter
+end
+
 RSpec.describe NullFieldsCounter do
+  let(:dc) { DummyClass.new }
   describe '.count_null_fields' do
     it 'should raise an error when params are missing' do
-      expect { NullFieldsCounter.count_null_fields }.to raise_error(ArgumentError)
+      expect { DummyClass.count_null_fields }.to raise_error(ArgumentError)
     end
 
     it 'should return an error message when the column does not exist' do
-      expect(NullFieldsCounter.count_null_fields('Case', 'blah')).to include('Error')
+      expect(DummyClass.count_null_fields('Case', 'blah')).to include('Error')
     end
 
     it 'should return an error message if the model does not exist' do
-      expect(NullFieldsCounter.count_null_fields('Blah', 'blah')).to include('Error')
+      expect(DummyClass.count_null_fields('Blah', 'blah')).to include('Error')
     end
   end
 end
