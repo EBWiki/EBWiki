441aedd1adaa59a2fd7a2a0c6ecef8eaf4f1ee6f
Rachel Green <rlgreen91@gmail.com>
2021-09-28 20:27:38 -0500

    Fixes spec/models/follow_spec for 5.2 upgrade
    Fixes spec/models/follow_spec for the 5.2 upgrade.  Removes the follow factory as it is no longer needed.

--- spec/models/follow_spec.rb
+++ spec/models/follow_spec.rb
@@ -4,9 +4,14 @@ require 'rails_helper'
 
 RSpec.describe Follow do
   describe 'scopes' do
+    let(:user) { create(:user) }
+    let(:test_case) { create(:case, user: user) }
+
+    before(:each) { user.follow(test_case) }
+
     it 'returns follows occurring by a given date' do
-      follow = FactoryBot.create(:follow)
-      follow.update(created_at: 17.days.ago)
+      follow = Follow.first
+      follow.update!(created_at: 17.days.ago)
       expect(Follow.occurring_by(15.days.ago).to_a).to include(follow)
     end
   end
