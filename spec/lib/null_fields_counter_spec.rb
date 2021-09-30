22f6f38d9146cec7c9e569a246952ad234df110d
Mark Nyon <mnyon@2u.com>
2021-09-29 20:51:18 -0400

    Fix null fields counter spec #4037

--- spec/lib/null_fields_counter_spec.rb
+++ spec/lib/null_fields_counter_spec.rb
@@ -1,24 +1,21 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'null_fields_counter'
 
-class CounterClass
-  include NullFieldsCounter
-end
 
 RSpec.describe NullFieldsCounter do
-  let(:dc) { DummyClass.new }
   describe '.count_null_fields' do
     it 'should raise an error when params are missing' do
-      expect { DummyClass.count_null_fields }.to raise_error(ArgumentError)
+      expect { NullFieldsCounter.count_null_fields }.to raise_error(ArgumentError)
     end
 
     it 'should return an error message when the column does not exist' do
-      expect(DummyClass.count_null_fields('Case', 'blah')).to include('Error')
+      expect(NullFieldsCounter.count_null_fields('Case', 'blah')).to include('Error')
     end
 
     it 'should return an error message if the model does not exist' do
-      expect(DummyClass.count_null_fields('Blah', 'blah')).to include('Error')
+      expect(NullFieldsCounter.count_null_fields('Blah', 'blah')).to include('Error')
     end
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -12,6 +12,7 @@ require 'rspec/rails'
 require 'database_cleaner'
 require 'webmock/rspec'
 require 'paper_trail/frameworks/rspec'
+require 'null_fields_counter'
 include Warden::Test::Helpers
 Warden.test_mode!
 # Requires supporting ruby files with custom matchers and macros, etc,
@@ -29,7 +30,7 @@ FactoryBot.use_parent_strategy = false
 RSpec.configure do |config|
   # Factory Girl
   config.include FactoryBot::Syntax::Methods
-  
+
     # Include Devise test helpers
   config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include Devise::Test::IntegrationHelpers, type: :request
