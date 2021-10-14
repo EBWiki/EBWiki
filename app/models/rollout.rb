4f6d275cf68bc71d91e0666e8efc4a447fc59b2a
Oliver Kristen <69202126+olikristen@users.noreply.github.com>
2021-10-14 17:52:31 +0200

    Use inheritance from ValueObject

--- app/models/rollout.rb
+++ app/models/rollout.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 # Value object - rollout
-class Rollout
+class Rollout < ValueObject
   attr_reader :name, :description, :creator, :date_added
 
-  def initialize(path_to_yaml)
+  def initialize(path_to_yaml) # rubocop:disable Lint/MissingSuper
     file = YAML.load_file(path_to_yaml)
     @name = file['name']
     @description = file['description']
@@ -13,10 +13,6 @@ class Rollout
   end
 
   def to_s
-    "#{name}"
-  end
-
-  def ==(other)
-    self.class == other.class && self.to_s == other.to_s
+    name.to_s
   end
 end
