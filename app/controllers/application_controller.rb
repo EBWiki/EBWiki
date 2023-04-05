4a090d21385920a05a0224f211af1f610001dc54
Mark Nyon <msnyon@hey.com>
2023-04-05 11:47:05 -0600

    Removed paper_trail.whodunnit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,22 +16,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception, prepend: true
-  before_action :set_paper_trail_whodunnit
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :state_objects
-  before_action :set_paper_trail_whodunnit
 
   helper_method :mailbox, :conversation
-
-  def info_for_paper_trail
-    # Save additional info
-    { ip: request.remote_ip }
-  end
-
-  def user_for_paper_trail
-    # Save the user responsible for the action
-    user_signed_in? ? current_user.id : 'Guest'
-  end
 
   private
 
