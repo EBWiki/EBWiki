7f186c5f4d3097beaf203ce3a2c84d01ab665e5c
Mark Nyon <msnyon@hey.com>
2023-04-05 09:40:11 -0600

    PaperTrail configuration options

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,2 +1,11 @@
 require "paper_trail/version"
+require "paper_trail/version_concern"
+
+PaperTrail::Rails::Engine.eager_load!
+include PaperTrail::VersionConcern
+module PaperTrail
+  class Version < ActiveRecord::Base
+    PaperTrail.config.track_associations = true
+  end
+end
 PaperTrail.config.track_associations = true
