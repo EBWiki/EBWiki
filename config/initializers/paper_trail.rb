5b700c3608f18348e70c6bc3b0efd2164b4b4311
Mark Nyon <msnyon@hey.com>
2023-04-04 19:36:08 -0600

    Clean initialization file

--- config/initializers/paper_trail.rb
+++ config/initializers/paper_trail.rb
@@ -1,10 +1,1 @@
-# frozen_string_literal: true
-
-# Config settings for paper_trail gem
-# PaperTrail::Rails::Engine.eager_load!
-include PaperTrail::VersionConcern
-module PaperTrail
-  class Version < ActiveRecord::Base
-    PaperTrail.config.track_associations = true
-  end
-end
+PaperTrail.config.track_associations = true
