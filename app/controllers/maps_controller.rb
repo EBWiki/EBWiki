9196c7c201c0dcb031b42a9c613c515694ba30d7
Mark Nyon <msnyon@hey.com>
2023-05-10 11:30:38 -0400

    Called preload app to deal with servers

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -4,22 +4,9 @@
 class MapsController < ApplicationController
   include MapsHelper
   before_action :authenticate_user!, except: %i[index show history followers]
-<<<<<<< HEAD
 
   def index
     @cases = fetch_cases
-=======
-  before_action :set_instance_vars, only: %i[edit new create]
-  def index
-    @cases = fetch_cases
-    @cases = @cases.map do |this_case|
-      [
-        this_case['id'], this_case['latitude'],
-        this_case['longitude'], this_case['avatar'],
-        this_case['title'], this_case['overview']
-      ]
-    end
->>>>>>> Initial maps controller #851
   end
 
   def after_sign_up_path_for(resource)
--- app/helpers/maps_helper.rb
+++ app/helpers/maps_helper.rb
@@ -5,7 +5,6 @@ module MapsHelper
   def fetch_cases
     cases = $redis.get('cases')
 
-<<<<<<< HEAD
     if cases.blank?
       case_locations = location_data(Case.with_location)
       $redis.set('cases', case_locations)
@@ -20,13 +19,5 @@ module MapsHelper
     cases.map do |case_location|
       [case_location.latitude, case_location.longitude]
     end.flatten.to_json
-=======
-    if cases.nil?
-      cases = Case.all.to_json
-      $redis.set('cases', cases)
-      $redis.expire('cases', 2.hour.to_i)
-    end
-    JSON.load cases
->>>>>>> Initial maps controller #851
   end
 end
