8fe1c460977559eb7c8c75e0df8359821c7f4e45
Mark Nyon <msnyon@hey.com>
2025-07-16 21:10:26 -0400

    Fix: Update mailer routes for 6.1

--- app/helpers/user_mailer_helper.rb
+++ app/helpers/user_mailer_helper.rb
@@ -2,16 +2,16 @@
 
 # Set of helper methods for composing email messages
 module UserMailerHelper
-  FOLLOW_CALL_TO_ACTION = I18n.t 'notifier.follow_cases'
+  FOLLOW_CALL_TO_ACTION = I18n.t('notifier.follow_cases')
+  SUBSCRIBER_MESSAGE = I18n.t('notifier.subscribe_message')
 
-  SUBSCRIBER_MESSAGE = I18n.t 'notifier.subscribe_message'
-
-  subscriber_link_copy = 'Subscribe to our newsletter as well'
-  subscriber_link = ActionController::Base.helpers.link_to(
-    subscriber_link_copy, ENV.fetch('MAILCHIMP_LINK', nil)
-  )
-  SUBSCRIBER_CALL_TO_ACTION = "#{subscriber_link} for periodic" \
-                              'general updates and commentaries on this issue.'
+  def subscriber_call_to_action
+    subscriber_link_copy = 'Subscribe to our newsletter as well'
+    subscriber_link = ActionController::Base.helpers.link_to(
+      subscriber_link_copy, ENV.fetch('MAILCHIMP_LINK', nil)
+    )
+    "#{subscriber_link} for periodic general updates and commentaries on this issue."
+  end
 
   def create_welcome_email_message(user, is_subscribed)
     followed_cases = user.all_following.count
@@ -21,14 +21,14 @@ module UserMailerHelper
   def follow_cases_message(followed_cases)
     return FOLLOW_CALL_TO_ACTION if followed_cases.zero?
 
-    "You have already taken the first step by following \
-      #{pluralize(followed_cases, 'case')} \
-      on EBWiki and allowing us to keep you up to date."
+    "You have already taken the first step by following " \
+      "#{pluralize(followed_cases, 'case')} " \
+      "on EBWiki and allowing us to keep you up to date."
   end
 
   def subscribe_message(is_subscribed)
     return SUBSCRIBER_MESSAGE if is_subscribed
 
-    SUBSCRIBER_CALL_TO_ACTION
+    subscriber_call_to_action
   end
 end
