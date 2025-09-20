787963c1b9f3ab5aae169ab19bcfdd95483dd831
Mark Nyon <msnyon@hey.com>
2025-09-20 03:57:42 -0400

    Configure Rails 7.0 defaults and database schema format
    - Update config.load_defaults from 6.1 to 7.0
    - Add require 'logger' to fix Logger ThreadSafeLevel issue
    - Configure active_record.schema_format = :sql to use structure.sql
    - This resolves the concurrent-ruby compatibility issue with Rails 7

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,6 @@
 require_relative 'boot'
 
+require 'logger'
 require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
@@ -9,7 +10,10 @@ Bundler.require(*Rails.groups)
 module EBWiki
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 6.1
+    config.load_defaults 7.0
+
+    # Use structure.sql instead of schema.rb for database schema
+    config.active_record.schema_format = :sql
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
