1f04b9fdffd4466b382198939598649fb4a19282
Mark Nyon <msnyon@hey.com>
2025-10-28 05:26:33 -0400

    on #4288: finished Rubocop fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -39,7 +39,7 @@ class ApplicationController < ActionController::Base
   private
 
   def set_state_objects
-    @state_objects ||= SortCollectionOrdinally.call(collection: State.all)
+    @set_state_objects ||= SortCollectionOrdinally.call(collection: State.all)
   end
 
   def log_invalid_token_attempt(exception)
--- config/initializers/rollout.rb
+++ config/initializers/rollout.rb
@@ -1,3 +1,3 @@
 # frozen_string_literal: true
 
-$rollout = Rollout.new($redis)
+# $rollout = Rollout.new($redis)
