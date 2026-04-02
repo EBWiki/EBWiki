fcbb060d60b671f17be6f818b732a0973e4aa7c4
Mark Nyon <msnyon@hey.com>
2026-04-02 01:54:08 -0400

    Replace RailsAdmin with Administrate for admin dashboard
    RailsAdmin has had recurring compatibility issues across Rails upgrades.
    Administrate is better maintained and integrates more naturally with
    standard Rails conventions.
    
    - Add administrate gem, remove rails_admin gem
    - Generate Administrate dashboards for all 14 models
    - Generate namespaced admin controllers under Admin::
    - Implement admin authentication matching prior RailsAdmin behavior
      (Devise/Warden-based, redirects non-admins with flash message)
    - Remove sensitive fields from UserDashboard (encrypted_password,
      reset_password_token, confirmation_token, IPs)
    - Fix CaseDashboard: deduplicate :state, use BelongsTo for state
      association, curate COLLECTION/SHOW/FORM attribute lists
    - Update header partial to link to admin_root_path
    - Rewrite admin feature specs for Administrate paths
    - Delete config/initializers/rails_admin.rb
    - Clean up routes.rb (proper indentation, remove stale TODO)
    
    Closes #4046 (admin deletion verification can now use Administrate)
    Refs #4304 (rails_admin removed as part of Rails 8.1 compatibility)

---
+++ app/dashboards/calendar_event_dashboard.rb
@@ -0,0 +1,90 @@
+# frozen_string_literal: true
+
+require 'administrate/base_dashboard'
+
+class CalendarEventDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    city: Field::String,
+    description: Field::Text,
+    format: Field::Select.with_options(searchable: false, collection: lambda { |field|
+      field.resource.class.send(field.attribute.to_s.pluralize).keys
+    }),
+    links: Field::HasMany,
+    owner_id: Field::Number,
+    schedule: Field::String.with_options(searchable: false),
+    state: Field::String,
+    street_address: Field::String,
+    title: Field::String,
+    zipcode: Field::String
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = %i[
+    id
+    city
+    description
+    format
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[
+    id
+    city
+    description
+    format
+    links
+    owner_id
+    schedule
+    state
+    street_address
+    title
+    zipcode
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[
+    city
+    description
+    format
+    links
+    owner_id
+    schedule
+    state
+    street_address
+    title
+    zipcode
+  ].freeze
+
+  # COLLECTION_FILTERS
+  # a hash that defines filters that can be used while searching via the search
+  # field of the dashboard.
+  #
+  # For example to add an option to search for open resources by typing "open:"
+  # in the search field:
+  #
+  #   COLLECTION_FILTERS = {
+  #     open: ->(resources) { resources.where(open: true) }
+  #   }.freeze
+  COLLECTION_FILTERS = {}.freeze
+
+  # Overwrite this method to customize how calendar events are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(calendar_event)
+  #   "CalendarEvent ##{calendar_event.id}"
+  # end
+end
---
+++ app/dashboards/user_dashboard.rb
@@ -0,0 +1,114 @@
+# frozen_string_literal: true
+
+require 'administrate/base_dashboard'
+
+class UserDashboard < Administrate::BaseDashboard
+  # ATTRIBUTE_TYPES
+  # a hash that describes the type of each of the model's fields.
+  #
+  # Each different type represents an Administrate::Field object,
+  # which determines how the attribute is displayed
+  # on pages throughout the dashboard.
+  ATTRIBUTE_TYPES = {
+    id: Field::Number,
+    admin: Field::Boolean,
+    analyst: Field::Boolean,
+    city: Field::String,
+    comments: Field::HasMany,
+    confirmed_at: Field::DateTime,
+    current_sign_in_at: Field::DateTime,
+    description: Field::Text,
+    email: Field::String,
+    facebook_url: Field::String,
+    follows: Field::HasMany,
+    last_sign_in_at: Field::DateTime,
+    latitude: Field::Number.with_options(decimals: 2),
+    linkedin: Field::String,
+    longitude: Field::Number.with_options(decimals: 2),
+    messages: Field::HasMany,
+    name: Field::String,
+    sign_in_count: Field::Number,
+    slug: Field::String,
+    state: Field::String,
+    state_id: Field::Number,
+    subscribed: Field::Boolean,
+    twitter_url: Field::String,
+    created_at: Field::DateTime,
+    updated_at: Field::DateTime
+  }.freeze
+
+  # COLLECTION_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's index page.
+  #
+  # By default, it's limited to four items to reduce clutter on index pages.
+  # Feel free to add, remove, or rearrange items.
+  COLLECTION_ATTRIBUTES = %i[
+    id
+    name
+    email
+    admin
+  ].freeze
+
+  # SHOW_PAGE_ATTRIBUTES
+  # an array of attributes that will be displayed on the model's show page.
+  SHOW_PAGE_ATTRIBUTES = %i[
+    id
+    name
+    email
+    admin
+    analyst
+    city
+    state
+    description
+    facebook_url
+    twitter_url
+    linkedin
+    comments
+    follows
+    messages
+    sign_in_count
+    current_sign_in_at
+    last_sign_in_at
+    confirmed_at
+    subscribed
+    slug
+    created_at
+    updated_at
+  ].freeze
+
+  # FORM_ATTRIBUTES
+  # an array of attributes that will be displayed
+  # on the model's form (`new` and `edit`) pages.
+  FORM_ATTRIBUTES = %i[
+    name
+    email
+    admin
+    analyst
+    city
+    state
+    description
+    facebook_url
+    twitter_url
+    linkedin
+    subscribed
+  ].freeze
+
+  # COLLECTION_FILTERS
+  # a hash that defines filters that can be used while searching via the search
+  # field of the dashboard.
+  #
+  # For example to add an option to search for open resources by typing "open:"
+  # in the search field:
+  #
+  #   COLLECTION_FILTERS = {
+  #     open: ->(resources) { resources.where(open: true) }
+  #   }.freeze
+  COLLECTION_FILTERS = {}.freeze
+
+  # Overwrite this method to customize how users are displayed
+  # across all pages of the admin dashboard.
+  #
+  # def display_resource(user)
+  #   "User ##{user.id}"
+  # end
+end
